248152bab977249511b5f444d7b800f0704bc6a9
lynnaloo <lynnaloo@gmail.com>
2013-04-30 10:41:23 -0400

    Issue 20201: Updated buildExtensions.sh to include Orange.

--- tools/buildExtensions.sh
+++ tools/buildExtensions.sh
@@ -11,4 +11,9 @@ tools/deploy.sh
 mkdir builds/connect
 mv build/app.js builds/connect/connect.js
 
+cp source/orange/client/root-package.js package.js
+tools/deploy.sh
+mkdir builds/orange
+mv build/app.js builds/orange/orange.js
+
 rm package.js
