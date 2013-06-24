fcf893601453ae43f9d8809f83e6c321d451e091
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-06-23 21:57:03 -0400

    issue #20524: got incident plus working

--- source/incident_plus/database/source/manifest.js
+++ source/incident_plus/database/source/manifest.js
@@ -1,6 +1,12 @@
 {
   "version": "1.3.9",
+  "comment": "Found In / Fixed In Extension",
   "databaseScripts": [
+    "xtincdtpls/schema/xtincdtpls.sql",
+    "xtincdtpls/trigger_functions/incdtvertrigger.sql",
+    "xtincdtpls/tables/prjver.sql",
+    "xtincdtpls/tables/incdtver.sql",
+    "register.sql"
 
   ]
 }
