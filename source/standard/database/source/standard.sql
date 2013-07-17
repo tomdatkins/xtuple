39d1db720f2a81133d65e2c98f509093a77d27fb
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-07-17 13:46:37 -0400

    issue #19801: brought back user preferences options

--- source/standard/database/source/standard.sql
+++ source/standard/database/source/standard.sql
@@ -6,20 +6,23 @@ select xt.install_js('XM','Standard','standard', $$
 
   var salesSettings = [
       "MultiWhs"
+    ],
+    userPreferences = [
+      "PreferredWarehouse"
     ];
 
-  /* addresses a crash if this extension alone is being installed. In normal
-  life is not necessary because js_init() will call these other objects first */
-  if(!XM.Sales) {
-    XM.Sales = {
-      options: []
-    };
-  }
   salesSettings.map(function (setting) {
     if(!XM.Sales.options.contains(setting)) {
       XM.Sales.options.push(setting);
     }
   }); 
+
+  userPreferences.map(function (pref) {
+    if(!XM.UserPreference.options.contains(pref)) {
+      XM.UserPreference.options.push(pref);
+    }
+  }); 
+
 }());
 $$, true );
 
