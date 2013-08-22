cee41e253ad6a678ebaa20a6b0d2c5b92004b419
Ben Thompson <ben@xtuple.com>
2013-08-22 18:51:16 -0400

    Initial commit of xdruple extension with only db.

---
+++ source/xdruple/database/source/manifest.js
@@ -0,0 +1,11 @@
+{
+  "version": "1.4.2",
+  "comment": "xDruple Extension",
+  "databaseScripts": [
+    "xdruple/schema/xdruple.sql",
+    "xdruple/tables/xd_site.sql",
+    "xdruple/tables/xd_user_contact.sql",
+    "priv.sql",
+    "register.sql"
+  ]
+}
---
+++ source/xdruple/database/source/xdruple/tables/xd_site.sql
@@ -0,0 +1,12 @@
+-- table definitions
+
+select xt.create_table('xd_site', 'xdruple');
+select xt.add_column('xd_site','xd_site_id', 'serial', 'primary key', 'xdruple', 'xd_site table primary key.');
+select xt.add_column('xd_site','xd_site_name', 'text', 'not null unique', 'xdruple', 'The Drupal site''s name');
+select xt.add_column('xd_site','xd_site_url', 'text', 'not null unique', 'xdruple', 'The Drupal site''s URL');
+select xt.add_column('xd_site','xd_site_notes', 'text', null, 'xdruple', 'The Drupal site''s notes');
+-- TODO: Add more columns to define OAuth 2.0 Client settings needed to connect to the Drupal site.
+-- This would allow for two way integration with the Drupal site so we could request the User object
+-- and see their details from an xTuple client (Qt/Mobile). Currently, all we will see is the uid.
+
+comment on table xdruple.xd_site is 'Defines Drupal sites that are integrated with xTuple';
