9f25c1cb0c84efb535fad19fb311f91ed47e7c3d
Ben Thompson <ben@xtuple.com>
2013-09-11 19:14:39 -0400

    Issue #20571 Add Drupal Commerce commerce_price field  integration support ORM.

--- source/xdruple/database/source/manifest.js
+++ source/xdruple/database/source/manifest.js
@@ -5,6 +5,7 @@
     "xdruple/schema/xdruple.sql",
     "xdruple/tables/xd_site.sql",
     "xdruple/tables/xd_user_contact.sql",
+    "xdruple/tables/xd_field_data_dc_price.sql",
     "priv.sql",
     "register.sql"
   ]
