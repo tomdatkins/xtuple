b1194ad9909390cc3878be446235fb0046cf58e5
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-09-18 13:17:03 -0400

    register extensions and dependencies in build tool

--- source/incident_plus/database/source/manifest.js
+++ source/incident_plus/database/source/manifest.js
@@ -1,13 +1,13 @@
 {
-  "version": "1.4.0",
+  "name": "incident_plus",
+  "version": "1.4.5",
   "comment": "Found In / Fixed In Extension",
+  "loadOrder": 120,
   "dependencies": ["crm"],
   "databaseScripts": [
     "xtincdtpls/schema/xtincdtpls.sql",
     "xtincdtpls/trigger_functions/incdtvertrigger.sql",
     "xtincdtpls/tables/prjver.sql",
-    "xtincdtpls/tables/incdtver.sql",
-    "register.sql"
-
+    "xtincdtpls/tables/incdtver.sql"
   ]
 }
