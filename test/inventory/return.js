9720fb58aa99ba7c0bdc077f01ddbd87586d8de5
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-12-11 13:06:08 -0500

    issue #22067: start to private-extensions test framework

---
+++ test/inventory/return.js
@@ -0,0 +1,23 @@
+/*jshint indent:2, curly:true, eqeqeq:true, immed:true, latedef:true,
+newcap:true, noarg:true, regexp:true, undef:true, strict:true, trailing:true,
+white:true*/
+/*global XV:true, XT:true, _:true, console:true, XM:true, Backbone:true,
+require:true, assert:true, setTimeout:true, clearTimeout:true, exports:true,
+it:true, describe:true, beforeEach:true, before:true, enyo:true */
+
+(function () {
+  "use strict";
+
+  var coreReturn = require("../../../xtuple/test/specs/return");
+  var additionalTests = function () {
+    describe("Inventory extensions to return", function () {
+      it("is fine", function () {
+
+      });
+    });
+  };
+
+  exports.spec = coreReturn.spec;
+  exports.additionalTests = additionalTests;
+}());
+
---
+++ test/lib/test_runner.js
@@ -0,0 +1,26 @@
+/*jshint trailing:true, white:true, indent:2, strict:true, curly:true,
+  immed:true, eqeqeq:true, forin:true, latedef:true,
+  newcap:true, noarg:true, undef:true */
+/*global XT:true, XM:true, XV:true, exports:true, describe:true, it:true,
+require:true, __dirname:true, console:true */
+
+(function () {
+  "use strict";
+
+  var fs = require('fs'),
+    _ = require("underscore"),
+    path = require('path'),
+    // TODO: not just inventory
+    specFiles = _.filter(fs.readdirSync(path.join(__dirname, "../inventory")), function (fileName) {
+      // filter out .swp files, etc.
+      return path.extname(fileName) === '.js';
+    }),
+    specs = _.map(specFiles, function (specFile) {
+      return require(path.join(__dirname, "../inventory", specFile));
+    }),
+    runSpec = require("../../../xtuple/test/lib/runner_engine").runSpec;
+
+  _.each(specs, runSpec);
+
+}());
+
