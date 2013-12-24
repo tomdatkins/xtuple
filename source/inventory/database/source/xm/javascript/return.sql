3354038a52e55e31e474d197aed0fc25e68ee0a4
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-12-23 21:13:00 -0500

    issue #22067: work towards distribution

--- source/inventory/database/source/xm/javascript/return.sql
+++ source/inventory/database/source/xm/javascript/return.sql
@@ -38,21 +38,46 @@ select xt.install_js('XM','Return','inventory', $$
       controlledLines = orderLines.filter(function (line) {
         return line.options.detail;
       });
+
     controlledLines.map(function (orderLine) {
       plv8.execute(setUpdateInvSql, [orderLine.orderLine, false]);
     });
 
-    /* step 2: run the post function 
+    /* step 2: run the post function */
     XM.Return.post(returnNumber);
-    */
 
     /* step 3: reinstate the updateInventory values */
     controlledLines.map(function (orderLine) {
       plv8.execute(setUpdateInvSql, [orderLine.orderLine, true]);
     });
 
-    /* step 4: run the receipt function */
+    /* step 4: run the distribution function */
     controlledLines.map(function (orderLine) {
+      var series = plv8.execute("select nextval('itemloc_series_seq') as series")[0].series,
+        detailSql = "select itemsite_id, costcat_asset_accnt_id, item_number, " +
+          "cmitem_qtyreturned, item_number, " +
+          "from cmitem " +
+          "inner join itemsite on cmitem_itemsite_id = itemsite_id " +
+          "inner join item on itemsite_item_id = item_id " +
+          "inner join costcat on itemsite_costcat_id = costcat_id " +
+          "where cmitem.obj_uuid = $1;",
+        detail = plv8.execute(detailSql, [orderLine.orderLine])[0];
+
+      XT.executeFunction("postInvTrans", [
+        itemsite_id, 
+        'RS', 
+        detail.cmitem_qtyreturned,
+        'S/O', 
+        'CM', 
+        returnNumber, 
+        '',
+        'Credit Return ' + detail.item_number,
+        detail.costcat_asset_accnt_id,
+        null, /*getPrjAccntId(_r.cmhead_prj_id, resolveCOSAccount(itemsite_id, _r.cust_id, _r.saletype_id, _r.shipzone_id)), */
+        series, 
+        null, /*_glDate, */
+        _r.std_cost
+      ]);
       XM.PrivateInventory.distribute(series, orderLine);
     });
   };
