53c3a6724d8d3c2f25d826ecbcc522039038ebcf
John Rogelstad <john@xtuple.com>
2014-02-02 11:10:52 -0500

    issue #22534: Fix some problems with workbench operation and history.

--- source/inventory/client/views/list_relations_box.js
+++ source/inventory/client/views/list_relations_box.js
@@ -56,15 +56,23 @@ trailing:true, white:true*/
           list.setQuery({
             parameters: [
               {attribute: "itemSite.item.number", value: value}
+            ],
+            orderBy: [
+              {attribute: "transactionDate", descending: true},
+              {attribute: "created", descending: true},
+              {attribute: "uuid"},
             ]
           });
-          list.collectionChanged();
           list.fetch();
         } else {
           collection.reset();
+          collection.fetchCount += 1;  // Ignore previous requests
+          list.setCount(0);
+          list.reset();
         }
       },
-      updateButtons: function () {} // Not compatible with XV.List
+      updateButtons: function () {}, // Not compatible with XV.List
+      valueChanged: function () {} // Not compatible with XV.List
     });
 
     // ..........................................................
