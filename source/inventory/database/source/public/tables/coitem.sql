b414428d24a8b91f433944bc501f6a6d0e007dbd
John Rogelstad <john@xtuple.com>
2014-02-10 06:34:50 -0500

    issue #22669: Do not need to handle dlete in trigger now that view does it for us.

--- source/inventory/database/source/public/tables/coitem.sql
+++ source/inventory/database/source/public/tables/coitem.sql
@@ -1,3 +1,3 @@
 drop trigger if exists coitem_order_id_did_change on coitem;
-create trigger coitem_order_id_did_change after insert or update or delete on coitem for each row
+create trigger coitem_order_id_did_change after insert or update on coitem for each row
   execute procedure xt.coitem_order_id_did_change();
