a22111e9a43db8880f39986394d3d28e53a62278
John Rogelstad <john@xtuple.com>
2014-02-10 06:59:14 -0500

    issue #22669: Add support for site calendar on sales order.

--- source/manufacturing/client/models/sales_order.js
+++ source/manufacturing/client/models/sales_order.js
@@ -86,7 +86,59 @@ white:true*/
           status: status
         });
       }
+    });
 
+    // Order of events matters here
+    var _proto = XM.SalesOrderLine.prototype,
+      _scheduleDateChanged = _proto.scheduleDateChanged;
+
+    _.extend(_proto, {
+      /**
+        Implement site calendar.
+      */
+      scheduleDateChanged: function (model, changes, options) {
+        options = options ? _.clone(options) : {};
+        var scheduleDate = this.get("scheduleDate"),
+          site = this.get("site"),
+          useSiteCalendar = XT.session.settings.get("UseSiteCalendar"),
+          dispOptions = {},
+          nextDate,
+          that = this,
+          params,
+
+          afterCalculate = function (resp) {
+            var message = "_nextWorkingDate?".loc();
+
+            nextDate = new Date(resp);
+
+            // If dates don't match, ask if  they should
+            if (XT.date.compare(scheduleDate, nextDate)) {
+              that.notify(message, {
+                type: XM.Model.QUESTION,
+                callback: afterQuestion
+              });
+            } else {
+              _scheduleDateChanged.apply(that, arguments);
+            }
+          },
+
+          afterQuestion = function (resp) {
+            if (resp.answer) {
+              that.set("scheduleDate", nextDate, {validate: false});
+            } else {
+              _scheduleDateChanged.apply(that, arguments);
+            }
+          };
+
+        // Determine whether we need to get the server to answer some questions
+        if (useSiteCalendar && options.validate !== false) {
+          params = [site.id, scheduleDate, 0];
+          dispOptions.success = afterCalculate;
+          this.dispatch("XM.Site", "calculateNextWorkingDate", params, dispOptions);
+        } else {
+          _scheduleDateChanged.apply(this, arguments);
+        }
+      }
     });
 
   };
