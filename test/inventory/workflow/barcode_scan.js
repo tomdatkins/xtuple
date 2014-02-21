632bcc62fe73e820ab99737085ea81f78f3df18b
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-02-21 14:30:23 -0500

    issue #22459: clean up test

--- test/inventory/workflow/barcode_scan.js
+++ test/inventory/workflow/barcode_scan.js
@@ -16,48 +16,72 @@
     });
 
     describe('Issue to shipping with barcode scanner', function () {
+      var navigator,
+        postbooks,
+        orderWidget;
+
       this.timeout(40 * 1000);
       crud.runAllCrud(salesOrder.spec); // TODO: unknown why this is necessary
       salesOrder.spec.captureObject = true;
       smoke.runUICrud(salesOrder.spec);
-      it('should issue that sales order to shipping', function (done) {
-        var navigator = smoke.navigateToList(XT.app, "XV.TransferOrderList"),
-          postbooks = XT.app.$.postbooks,
-          orderWidget;
+
+      it("should open up the search screen in issueToShipping", function (done) {
+        navigator = smoke.navigateToList(XT.app, "XV.TransferOrderList");
+        postbooks = XT.app.$.postbooks;
+
         navigator.actionSelected(navigator.$.menuDecorator, {originator: {action: {method: "issueToShipping"}}});
         orderWidget = postbooks.$.issueToShipping.$.parameterWidget.$.order.$.input;
         orderWidget.menuItemSelected({}, {originator: orderWidget.$.searchItem});
-        setTimeout(function () {
-          // TODO: get rid of the setTimeouts
-          var model = postbooks.getActive().$.list.value.find(function (model) {
-            // fragility: what if there are more sales orders than are lazy-loaded?
-            return model.get("number") === XG.capturedId;
-          });
-          assert.isObject(model);
+        postbooks.getActive().$.list.value.once("status:READY_CLEAN", function () {
+          done();
+        });
+      });
 
-          postbooks.getActive().itemTap({}, {model: model});
-          setTimeout(function () {
-            console.log(postbooks.getActive().kind);
-            var transactionList = postbooks.getActive().$.list;
-            transactionList.captureBarcode({}, {data: "1234-4567"});
-            setTimeout(function () {
-              var workspaceContainer = postbooks.getActive();
-              workspaceContainer.$.workspace.value.set({toIssue: 99});
-              workspaceContainer.saveAndClose({force: true});
-              // ugly: blow through error message
-              XT.app.$.postbooks.notifyTap({}, {originator: {name: "notifyYes"}});
-              // TODO: ship
-              setTimeout(function () {
-                assert.equal(postbooks.getActive().kind, "XV.IssueToShipping");
-                postbooks.goToNavigator();
-                assert.equal(postbooks.getActive().kind, "XV.Navigator");
-                done();
-              }, 2000);
-            }, 2000);
-          }, 2000);
+      it("taps on the sales order we've created", function (done) {
+        var model = postbooks.getActive().$.list.value.find(function (model) {
+          // fragility: what if there are more sales orders than are lazy-loaded?
+          return model.get("number") === XG.capturedId;
+        });
+        assert.isObject(model);
+
+        postbooks.getActive().itemTap({}, {model: model});
+        postbooks.getActive().$.list.value.once("status:READY_CLEAN", function () {
+          done();
+        });
+      });
+
+      it("barcode-scans an item UPC code", function (done) {
+        var transactionList = postbooks.getActive().$.list;
+        transactionList.captureBarcode({}, {data: "1234-4567"});
+        //postbooks.getActive().$.workspace.value.on("all", function () {
+        //  console.log(arguments);
+        //});
+        // TODO: get rid of this setTimeout
+        setTimeout(function () {
+          done();
         }, 2000);
       });
 
+      it("commits the quantity to be issued", function (done) {
+        var workspaceContainer = postbooks.getActive();
+        workspaceContainer.$.workspace.value.set({toIssue: 99});
+        workspaceContainer.saveAndClose({force: true});
+        // ugly: blow through error message
+        XT.app.$.postbooks.notifyTap({}, {originator: {name: "notifyYes"}});
+        postbooks.getActive().$.list.value.on("status:READY_CLEAN", function () {
+          done();
+        });
+      });
+
+      it.skip("ships the shipment", function (done) {
+        // TODO: ship
+      });
+
+      it("backs out of the transaction list", function () {
+        assert.equal(postbooks.getActive().kind, "XV.IssueToShipping");
+        postbooks.goToNavigator();
+        assert.equal(postbooks.getActive().kind, "XV.Navigator");
+      });
     });
   });
 }());
