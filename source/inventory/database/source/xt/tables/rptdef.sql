0cf6e8be3a9cff416c77ced1ec420e06f8cd3998
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-02-22 22:00:08 -0500

    issue #22241: finish first draft of location label

--- source/inventory/database/source/xt/tables/rptdef.sql
+++ source/inventory/database/source/xt/tables/rptdef.sql
@@ -5,12 +5,28 @@ select xt.add_report_definition('XM.Location', 0, $${
   },
   "headerElements": [
     {
-      "definition": [{"text": "_orderNumber", "label": true}],
-      "options": {"fontBold": true, "x": 0, "y": 180}
+      "definition": [{"text": "_location"}],
+      "options": {"x": 83, "y": 0}
     },
     {
-      "definition": [{"attr": "aisle"}],
-      "options": {"x": 500, "y": 60, "align": "right", "font": "./fonts/f39.ttf"}
+      "definition": [
+        {"attr": "aisle"},
+        {"attr": "rack"},
+        {"attr": "bin"},
+        {"attr": "location"}
+      ],
+      "transform": "arbl",
+      "options": {"x": 30, "y": 40, "fontSize": 36, "font": "./fonts/f39.ttf"}
+    },
+    {
+      "definition": [
+        {"attr": "aisle"},
+        {"attr": "rack"},
+        {"attr": "bin"},
+        {"attr": "location"}
+      ],
+      "transform": "arbl",
+      "options": {"fontBold": true, "x": 75, "y": 75}
     }
   ],
   "detailElements": [
