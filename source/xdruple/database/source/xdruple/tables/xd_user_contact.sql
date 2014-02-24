70d9f5241e96591d84a3e95dc5e12c45b2664a46
Ben Thompson <ben@xtuple.com>
2014-02-24 12:22:15 -0500

    Add create new customer flag to xdruple user to contact table.

--- source/xdruple/database/source/xdruple/tables/xd_user_contact.sql
+++ source/xdruple/database/source/xdruple/tables/xd_user_contact.sql
@@ -5,6 +5,7 @@ select xt.add_column('xd_user_contact','xd_user_contact_id', 'serial', 'primary 
 select xt.add_column('xd_user_contact','xd_user_contact_site_id', 'integer', 'not null references xdruple.xd_site (xd_site_id) on delete cascade', 'xdruple', 'Drupal site id for this association.');
 select xt.add_column('xd_user_contact','xd_user_contact_uid', 'integer', null, 'xdruple', 'Drupal user''s uid.');
 select xt.add_column('xd_user_contact','xd_user_contact_cntct_id', 'integer', 'not null', 'xdruple', 'xTuple Contact''s cntct_id');
+select xt.add_column('xd_user_contact','xd_user_contact_create_new_cust', 'boolean', 'not null default false', 'xdruple', 'Create a new xTuple Customer from this Contact. Set to false if this is an existing Contact.');
 select xt.add_column('xd_user_contact','obj_uuid', 'uuid', 'default xt.uuid_generate_v4()', 'xdruple');
 
 select xt.add_constraint('xd_user_contact','xd_user_contact_unique_uid_association', 'unique(xd_user_contact_site_id, xd_user_contact_uid)', 'xdruple');
--- source/xdruple/database/source/xdruple/triggers/xd_create_b2x_user.sql
+++ source/xdruple/database/source/xdruple/triggers/xd_create_b2x_user.sql
@@ -3,74 +3,76 @@ create or replace function xdruple.xd_create_b2x_user() returns trigger as $$
 return (function () {
 
   try {
-    var username = plv8.execute('select current_user as username')[0].username,
-      contact = plv8.execute("select * from cntct where cntct_id = $1;", [NEW.xd_user_contact_cntct_id])[0],
-      address = plv8.execute("select * from addr where addr_id = $1;", [contact.cntct_addr_id])[0],
-      customer = XM.Customer.defaults(),
-      cust_payload = {
-        "username": username,
-        "nameSpace":"XM",
-        "type": "Customer"
-      },
-      shipto_payload = {
-        "username": username,
-        "nameSpace":"XM",
-        "type": "XdrupleCommerceShipto"
-      },
-      user_payload = {
-        "username": username,
-        "nameSpace":"XM",
-        "type": "UserAccount"
+    if (NEW.xd_user_contact_create_new_cust) {
+      var username = plv8.execute('select current_user as username')[0].username,
+        contact = plv8.execute("select * from cntct where cntct_id = $1;", [NEW.xd_user_contact_cntct_id])[0],
+        address = plv8.execute("select * from addr where addr_id = $1;", [contact.cntct_addr_id])[0],
+        customer = XM.Customer.defaults(),
+        cust_payload = {
+          "username": username,
+          "nameSpace":"XM",
+          "type": "Customer"
+        },
+        shipto_payload = {
+          "username": username,
+          "nameSpace":"XM",
+          "type": "XdrupleCommerceShipto"
+        },
+        user_payload = {
+          "username": username,
+          "nameSpace":"XM",
+          "type": "UserAccount"
+        };
+
+      /* Create the Customer and therefore CRM Account. */
+      cust_payload.data = XM.Customer.defaults();
+      cust_payload.data.name = contact.cntct_first_name + " " + contact.cntct_last_name;
+      cust_payload.data.number = contact.cntct_email.toUpperCase();
+      cust_payload.data.billingContact = contact.cntct_number;
+
+      /* TODO: Set CRM Account as individual? Defaults to Organization.
+
+      /* POST the new Customer with defaults set. */
+      new_customer = XT.Rest.post(cust_payload);
+
+      /* Create a Default Ship To for the Customer form the Contact and Address. */
+      shipto_payload.data = {
+        "customer": new_customer.id,
+        "number": 'SHIP-TO-1',
+        "name": 'Ship To 1',
+        "isDefault": true,
+        "contact": contact.cntct_number,
+        "address": address.addr_number
       };
 
-    /* Create the Customer and therefore CRM Account. */
-    cust_payload.data = XM.Customer.defaults();
-    cust_payload.data.name = contact.cntct_first_name + " " + contact.cntct_last_name;
-    cust_payload.data.number = contact.cntct_email.toUpperCase();
-    cust_payload.data.billingContact = contact.cntct_number;
+      /* POST the new Ship To. */
+      new_shipto = XT.Rest.post(shipto_payload);
 
-    /* TODO: Set CRM Account as individual? Defaults to Organization.
+      /* Create a User Account. */
+      user_payload.data = {
+        "username": contact.cntct_email.toLowerCase(),
+        "properName": contact.cntct_first_name + " " + contact.cntct_last_name,
+        "useEnhancedAuth": true,
+        "disableExport": true,
+        "isActive": true,
+        "initials": contact.cntct_first_name.substring(0,1).toUpperCase() + contact.cntct_last_name.substring(0,1).toUpperCase(),
+        "email": contact.cntct_email.toLowerCase(),
+        "organization": XT.currentDb,
+        "locale": "Default",
+        "isAgent": false
+      };
 
-    /* POST the new Customer with defaults set. */
-    new_customer = XT.Rest.post(cust_payload);
+      /* POST the new User Account. */
+      new_user = XT.Rest.post(user_payload);
 
-    /* Create a Default Ship To for the Customer form the Contact and Address. */
-    shipto_payload.data = {
-      "customer": new_customer.id,
-      "number": 'SHIP-TO-1',
-      "name": 'Ship To 1',
-      "isDefault": true,
-      "contact": contact.cntct_number,
-      "address": address.addr_number
-    };
+      /* Set the username on the CRM Account. */
+      plv8.execute("update crmacct set crmacct_usr_username = $1 where crmacct_number = $2;", [new_user.id, new_customer.id]);
 
-    /* POST the new Ship To. */
-    new_shipto = XT.Rest.post(shipto_payload);
+      /* Set role/privs and ext for this user. */
+      plv8.execute("select grant_user_role($1, $2)", [new_user.id, 'XDRUPLE']);
 
-    /* Create a User Account. */
-    user_payload.data = {
-      "username": contact.cntct_email.toLowerCase(),
-      "properName": contact.cntct_first_name + " " + contact.cntct_last_name,
-      "useEnhancedAuth": true,
-      "disableExport": true,
-      "isActive": true,
-      "initials": contact.cntct_first_name.substring(0,1).toUpperCase() + contact.cntct_last_name.substring(0,1).toUpperCase(),
-      "email": contact.cntct_email.toLowerCase(),
-      "organization": XT.currentDb,
-      "locale": "Default",
-      "isAgent": false
-    };
-
-    /* POST the new User Account. */
-    new_user = XT.Rest.post(user_payload);
-
-    /* Set the username on the CRM Account. */
-    plv8.execute("update crmacct set crmacct_usr_username = $1 where crmacct_number = $2;", [new_user.id, new_customer.id]);
-
-    /* Set role/privs and ext for this user. */
-    plv8.execute("select grant_user_role($1, $2)", [new_user.id, 'XDRUPLE']);
-
-    return NEW;
+      return NEW;
+    }
   } catch (err) {
     XT.error(err);
   }
