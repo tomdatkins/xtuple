a49c6b3f9c67649d1780220d6e1dc7c1b0fe9b88
julesstp <julianmcarlin@gmail.com>
2014-03-04 11:50:54 -0600

    remove dup file

--- source/manufacturing/database/source/xt/functions/womatl_posted_value .sql
+++
@@ -1,6 +0,0 @@
-create or replace function xt.womatl_posted_value(integer) returns numeric stable as $$
-  select coalesce(sum(invhist_value_before - invhist_value_after),0)
-  from womatlpost
-    join invhist on invhist_id=womatlpost_invhist_id
-  where womatlpost_womatl_id=$1;
-$$ language sql;
