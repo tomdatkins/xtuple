4f523dfcbaa24a23ad26cf610c1514cfdddf840e
jgunderson <jgunderson@erpbisolutions.com>
2014-03-05 13:28:53 -0500

    22442, tag lines

--- source/bi/client/widgets/crm_funnel_chart.js
+++ source/bi/client/widgets/crm_funnel_chart.js
@@ -28,10 +28,10 @@ trailing:true, white:true*/
       "Actual Opportunities - "
     ],
     toolTips: [
-      "",
-      "",
-      "",
-      ""
+      " ",
+      " ",
+      " ",
+      " "
     ],
     query : "funnel",
     queryTemplates: [
@@ -100,11 +100,11 @@ trailing:true, white:true*/
       "All Bookings - "
     ],
     toolTips: [
-      "",
-      "",
-      "",
-      "",
-      ""
+      " ",
+      " ",
+      " ",
+      " ",
+      " "
     ],
     query : "funnel",
     queryTemplates: [
