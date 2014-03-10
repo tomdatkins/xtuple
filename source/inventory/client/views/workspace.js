778acc316b5c3f4a45442d6ccbbfa8f7a81b12d3
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-03-10 14:00:36 -0400

    issue #22782: hook report to enyo checkbox

--- source/inventory/client/views/workspace.js
+++ source/inventory/client/views/workspace.js
@@ -1229,12 +1229,8 @@ trailing:true, white:true, strict: false*/
       },
       create: function (options) {
         this.inherited(arguments);
-        var canPrint = this.getPrintAvailable(),
-          canBill = XT.session.privileges.get('SelectBilling'),
+        var canBill = XT.session.privileges.get('SelectBilling'),
           autoBill = canBill && XT.session.settings.get('AutoSelectForBilling');
-
-        this.$.printPacklist.setDisabled(!canPrint);
-        this.$.printPacklist.setChecked(canPrint);
 
         // XXX approveForBillingCheckbox for some reason cannot be called 'approveForBilling'.
         // The checkbox won't work. same goes for 'createInvoice'.
@@ -1261,6 +1257,9 @@ trailing:true, white:true, strict: false*/
         });
 
         this.inherited(arguments);
+        if (this.$.printPacklist.$.input.checked) {
+          this.print();
+        }
       }
     });
 
