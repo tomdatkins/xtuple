3c75d40f3761e769568906566d9a13c5865450ce
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-03-28 15:43:22 -0400

    issue #23086: add missing metric

--- source/inventory/foundation-database/dbscripts/misc/updateMetric.sql
+++ source/inventory/foundation-database/dbscripts/misc/updateMetric.sql
@@ -1,1 +1,6 @@
 UPDATE metric SET metric_value='Standard' WHERE metric_name='Application';
+
+UPDATE metric SET metric_value='t' WHERE metric_name='MultiWhs';
+insert into metric (metric_name, metric_value)
+select 'MultiWhs', 't'
+where not exists (select c.metric_id from metric c where c.metric_name = 'MultiWhs');
