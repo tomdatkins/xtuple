39b041158ed6ec86dde5b7b7800458e677200a56
julesstp <julianmcarlin@gmail.com>
2014-05-03 14:24:38 -0600

    force tests to run again with minor file change

--- source/inventory/client/en/strings.js
+++ source/inventory/client/en/strings.js
@@ -1,7 +1,7 @@
 /*jshint node:true, indent:2, curly:false, eqeqeq:true, immed:true,
 latedef:true, newcap:true, noarg:true, regexp:true, undef:true,
 strict:true, trailing:true, white:true */
-/*global XT:true */
+/*global XT:true  */
 
 (function () {
   "use strict";
