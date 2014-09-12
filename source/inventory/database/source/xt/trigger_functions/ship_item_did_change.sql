e0b70279e76b2f404fd0a36799def924339ddcae
Gil Moskowitz <gilmoskowitz@acm.org>
2014-09-12 17:48:30 -0400

    handle no-rows cases

--- source/inventory/database/source/xt/trigger_functions/ship_item_did_change.sql
+++ source/inventory/database/source/xt/trigger_functions/ship_item_did_change.sql
@@ -5,9 +5,9 @@ create or replace function xt.ship_item_did_change() returns trigger as $$
 return (function () {
 
   var shipitemId = TG_OP === 'DELETE' ? OLD.shipitem_id : NEW.shipitem_id;
-  
-  if (typeof XT === 'undefined') { 
-    plv8.execute("select xt.js_init();"); 
+
+  if (typeof XT === 'undefined') {
+    plv8.execute("select xt.js_init();");
   }
 
   /* replace monolithic query that gave the query optimizer fits with a short
@@ -16,59 +16,66 @@ return (function () {
                    "from shiphead " +
                    "join shipitem on shiphead_id=shipitem_shiphead_id " +
                    "where shipitem_id = $1;",
-    shipment = plv8.execute(sqlShipmentQ, [shipitemId]),
     sqlOrditemQ = "select orditem_ordhead_id " +
                   "from xt.orditem " +
                   "where (transacted_balance - at_dock) = 0 " +
                   "and orditem_id=$1;",
-    orditem  = plv8.execute(sqlOrditemQ,  [shipment[0].shipitem_orderitem_id]),
     sqlOrdheadQ = "select obj_uuid " +
                   "from xt.ordhead " +
                   "where ordhead_type = $1 " +
-                  "and ordhead_id in ($2)",
-    ordhead  = plv8.execute(sqlOrdheadQ,  [shipment[0].shipitem_order_type,
-                                           orditem.map(function (row) {
-                                             return row.orditem_ordhead_id;
-                                           }).join(",")]),
+                  "and ordhead_id in ({list});",
     sqlSuccessors = "select wf_completed_successors " +
         "from xt.wf " +
         "where wf_parent_uuid = $1 " +
-        "and wf_type = 'P' " + 
+        "and wf_type = 'P' " +
         "and wf_status in ('I', 'P');",
     sqlUpdate = "update xt.wf " +
         "set wf_status = 'C' " +
         "where wf_parent_uuid = $1 " +
-        "and wf_type = 'P' " + 
+        "and wf_type = 'P' " +
         "and wf_status in ('I', 'P');",
     sqlNotify = "select xt.workflow_notify(wf.obj_uuid) " +
         "from xt.wf " +
         "where wf_parent_uuid = $1 " +
-        "and wf_type = 'P' " + 
+        "and wf_type = 'P' " +
         "and wf_status in ('I', 'P');",
     sqlUpdateSuccessor = "update xt.wf " +
         "set wf_status = 'I' " +
-        "where obj_uuid = $1;"
+        "where obj_uuid = $1;",
+    shipment, orditem, ordhead
     ;
 
-  ordhead.map(function (row) {
-    var results = plv8.execute(sqlSuccessors, [row.uuid]);
+  shipment = plv8.execute(sqlShipmentQ, [shipitemId]);
+  if (shipment.length > 0) {    /* 0 or 1 row */
+    orditem = plv8.execute(sqlOrditemQ,  [shipment[0].shipitem_orderitem_id]);
+    if (orditem.length > 0) {   /* max num order types, sqlOrdheadQ limits to 0-1 */
+      ordhead  = plv8.execute(sqlOrdheadQ.replace(/{list}/g,
+                                                  orditem.map(function (row) {
+                                                    return row.orditem_ordhead_id;
+                                                  }).join(",")),
+                              [shipment[0].shipitem_order_type]);
 
-    /* Notify affected users */
-    var res = plv8.execute(sqlNotify, [row.uuid]);
-    
-    /* Update the workflow items */
-    plv8.execute(sqlUpdate, [row.uuid]);
+      ordhead.map(function (row) {
+        var results = plv8.execute(sqlSuccessors, [row.uuid]);
 
-    /* Update all the successors of all the workflow items */
-    results.map(function (result) {
-      if(result.wf_completed_successors) {
-        result.wf_completed_successors.split(",").map(function (successor) {
-          plv8.execute(sqlUpdateSuccessor, [successor]);
+        /* Notify affected users */
+        var res = plv8.execute(sqlNotify, [row.uuid]);
+
+        /* Update the workflow items */
+        plv8.execute(sqlUpdate, [row.uuid]);
+
+        /* Update all the successors of all the workflow items */
+        results.map(function (result) {
+          if(result.wf_completed_successors) {
+            result.wf_completed_successors.split(",").map(function (successor) {
+              plv8.execute(sqlUpdateSuccessor, [successor]);
+            });
+          }
         });
-      }
-    });
-    
-  });
+
+      });
+    }
+  }
 
   return TG_OP === 'DELETE' ? OLD : NEW;
 
