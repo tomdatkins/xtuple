47fb35dd828cfdf522bdaddf73b2e331d7071855
Ben Thompson <ben@xtuple.com>
2014-10-01 18:17:38 -0400

    Add ViewCharacteristics to XDRUPLE role.

--- source/xdruple/database/source/priv.sql
+++ source/xdruple/database/source/priv.sql
@@ -30,3 +30,4 @@ select xt.grant_role_priv('XDRUPLE', 'CRM', 'ViewPersonalOpportunities');
 select xt.grant_role_priv('XDRUPLE', 'CRM', 'ViewPersonalProjects');
 select xt.grant_role_priv('XDRUPLE', 'CRM', 'ViewPersonalToDoItems');
 select xt.grant_role_priv('XDRUPLE', 'Inventory', 'ViewWarehouses');
+select xt.grant_role_priv('XDRUPLE', 'Inventory', 'ViewCharacteristics');
