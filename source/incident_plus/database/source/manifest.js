7b80bce287c49e349d98c2a2db14ad9db31d5245
Gil Moskowitz <gmoskowitz@xtuple.com>
2014-10-30 13:13:24 -0400

    don't need frozen manifest; must ensure proper qt-client extension creation and load sequence

--- source/incident_plus/database/source/manifest.js
+++ source/incident_plus/database/source/manifest.js
@@ -1,14 +1,15 @@
 {
   "name": "incident_plus",
+  "defaultSchema": "xtincdtpls",
   "version": "1.4.5",
   "comment": "Found In / Fixed In Extension",
   "loadOrder": 120,
   "dependencies": ["crm", "project"],
   "databaseScripts": [
     "xtincdtpls/schema/xtincdtpls.sql",
-    "xtincdtpls/trigger_functions/incdtvertrigger.sql",
     "xtincdtpls/tables/prjver.sql",
     "xtincdtpls/tables/incdtver.sql",
+    "xtincdtpls/trigger_functions/incdtvertrigger.sql",
     "xtincdtpls/tables/pkgmetasql/incidents-xtuple.mql",
     "xtincdtpls/tables/pkgscript/incident.js",
     "xtincdtpls/tables/pkgscript/incidentWorkbench.js",
--- source/incident_plus/foundation-database/frozen_manifest.js
+++
@@ -1,8 +0,0 @@
-{
-  "name": "incident_plus_foundation_frozen",
-  "version": "",
-  "comment": "xTuple Incident Plus foundation",
-  "loadOrder": 3000,
-  "databaseScripts": [
-  ]
-}
