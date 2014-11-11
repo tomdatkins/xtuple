ef4c69c7798da6257ff72f576129b20ab1828fe3
garyhgohoos <gary@xtuple.com>
2014-11-11 11:43:46 -0500

    Issue #24829:create metrics if they don't exist

--- source/distribution/foundation-database/xwd/patches/setMetrics.sql
+++ source/distribution/foundation-database/xwd/patches/setMetrics.sql
@@ -1,9 +1,9 @@
 do $$
 begin
-if fetchMetricText('ServerVersion') < '4.9.0' then
-
+if not exists(select 1 from metric where metric_name='xwdUpdateActCost') then
   perform setMetric('xwdUpdateActCost', 't');
+end if;
+if not exists(select 1 from metric where metric_name='xwdAddPackBatch') then
   perform setMetric('xwdAddPackBatch', 't');
-
 end if;
 end$$;
