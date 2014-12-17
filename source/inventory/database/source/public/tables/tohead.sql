800a22b67d812712e840a4ff96fa67c5eccbe1b7
jcarlin <julianmcarlin@gmail.com>
2014-12-17 11:08:35 -0600

    table name in wftype table, tohead & wo table wf triggers

--- source/inventory/database/source/public/tables/tohead.sql
+++ source/inventory/database/source/public/tables/tohead.sql
@@ -2,3 +2,10 @@
 select xt.add_column('tohead','obj_uuid', 'uuid', 'default xt.uuid_generate_v4()', 'public');
 select xt.add_inheritance('tohead', 'xt.obj');
 select xt.add_constraint('tohead', 'tohead_obj_uuid','unique(obj_uuid)', 'public');
+
+-- auto workflow generation trigger
+drop trigger if exists towf_after_insert on tohead;
+create trigger towf_after_insert after insert on tohead for each row
+  execute procedure xt.createwf_after_insert();
+
+ALTER TABLE tohead DISABLE TRIGGER towf_after_insert;
--- source/manufacturing/database/source/public/tables/wo.sql
+++ source/manufacturing/database/source/public/tables/wo.sql
@@ -1,3 +1,10 @@
 drop trigger if exists wo_wf_did_change on wo;
 create trigger wo_wf_did_change after insert or delete or update on wo for each row
   execute procedure xt.wo_wf_did_change();
+
+-- auto workflow generation trigger
+drop trigger if exists wowf_after_insert on wo;
+create trigger wowf_after_insert after insert on wo for each row
+  execute procedure xt.createwf_after_insert();
+
+ALTER TABLE wo DISABLE TRIGGER wowf_after_insert;
