60719b4d67ae6232515472aa2ea189859b48374d
jcarlin <julianmcarlin@gmail.com>
2015-02-16 14:44:39 -0600

    resolves issue #2205 - sort IssueStock distribution detail list by expiration, created dates

--- source/inventory/database/source/xt/views/itemsitedtl.sql
+++ source/inventory/database/source/xt/views/itemsitedtl.sql
@@ -1,6 +1,8 @@
 select xt.create_view('xt.itemsitedtl', $$
    select itemloc.*,
-     0 as distributed
+     0 as distributed,
+     lsdetail_created
    from itemloc
-     join itemsite on itemsite_id = itemloc_itemsite_id;
+     join itemsite on itemsite_id = itemloc_itemsite_id
+     left join lsdetail on itemloc_ls_id = lsdetail_ls_id;
 $$);
