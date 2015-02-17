d467511d99ee142831dade9648b484634ccbbbfa
garyhgohoos <gary@xtuple.com>
2015-02-17 08:50:42 -0500

    Issue #23538:fix typo

--- foundation-database/public/functions/formatglaccount.sql
+++ foundation-database/public/functions/formatglaccount.sql
@@ -1,5 +1,5 @@
 
-CREATE OR REPLACE FUNCTION formatGLAccount(pAcctnid INTEGER) RETURNS TEXT IMMUTABLE AS $$
+CREATE OR REPLACE FUNCTION formatGLAccount(pAccntid INTEGER) RETURNS TEXT IMMUTABLE AS $$
 -- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 BEGIN
