a3d37669a3d6bedadcd10128e708126b33b26c14
jcarlin <julianmcarlin@gmail.com>
2015-02-18 17:58:34 -0600

    remove comment at end of sql file

--- database/source/public/tables/pohead.sql
+++ database/source/public/tables/pohead.sql
@@ -2,6 +2,3 @@
 select xt.add_column('pohead','obj_uuid', 'uuid', 'default xt.uuid_generate_v4()', 'public');
 select xt.add_inheritance('pohead', 'xt.obj');
 select xt.add_constraint('pohead', 'pohead_obj_uui_id','unique(obj_uuid)', 'public');
-
--- auto workflow generation trigger
--- Moved to xt.poheadext
