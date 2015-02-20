b6b14e0308f39d306d077e6de05e5198624f97a3
Greg Pazo <gpazo@xtuple.com>
2015-02-20 17:32:23 -0500

    move table creation to frozen, add to frozen_manifest.js and remove from manifest.js

--- source/commercialcore/foundation-database/frozen_manifest.js
+++ source/commercialcore/foundation-database/frozen_manifest.js
@@ -4,5 +4,11 @@
   "comment": "Commercial Core foundation",
   "loadOrder": 46,
   "databaseScripts": [
+    "frozen/create_xtcore_schema.sql",
+    "frozen/tables/emlprofile.sql",
+    "frozen/tables/poemlprofile.sql",
+    "frozen/tables/poheadext.sql",
+    "frozen/tables/potype.sql",
+    "frozen/tables/vendinfoext.sql"
   ]
 }
