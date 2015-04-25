1cab3ed49809f4574fd115eccafeea1d58981b67
Gil Moskowitz <gilmoskowitz@acm.org>
2015-04-25 11:38:25 -0400

    allow for different kinds of association using different abbreviations

--- foundation-database/public/functions/createdoctype.sql
+++ foundation-database/public/functions/createdoctype.sql
@@ -1,9 +1,10 @@
-create or replace function createDoctype(pEnum integer, pType text, pFull   text,
-                                         pTable text,   pKey  text, pNumber text,
-                                         pName  text,   pDesc text,
-                                         pWidget text = '', pJoin  text = '',
-                                         pParam  text = '', pUi    text = '',
-                                         pPriv   text = '')
+create or replace function createDoctype(pEnum integer, pType   text, pDocAss text,
+                                         pCharAss text, pFull   text, pTable  text,
+                                         pKey     text, pNumber text, pName   text,
+                                         pDesc    text,
+                                         pWidget  text = '', pJoin   text = '',
+                                         pParam   text = '', pUi     text = '',
+                                         pPriv    text = '')
   returns integer as $$
 /* Copyright (c) 1999-2015 by OpenMFG LLC, d/b/a xTuple. 
    See www.xtuple.com/CPAL for the full text of the software license.
@@ -12,15 +13,34 @@ create or replace function createDoctype(pEnum integer, pType text, pFull   text
    or update an existing record if it appears incomplete. The pEnum MUST match
    the corresponding Documents::DocumentSources (qt-client/widgets/documents.h)
    if the record type is there. Otherwise pass NULL.
+   For other params, NULL means use the existing value on update or default on insert.
    See the source table comments for a description of the other arguments.
  */
 declare
-  _id integer;
+  _justAddedCols boolean;
+  _id            integer := -1;
 begin
-  -- http://www.postgresql.org/docs/current/static/plpgsql-control-structures.html#PLPGSQL-UPSERT-EXAMPLE
-  loop
+  select (source_docass || source_charass = '') into _justAddedCols
+      from source where source_name = pType;
+  if NOT FOUND then
+    insert into source (source_enum, source_module,
+        source_name,      source_descrip,      source_table,
+        source_docass,    source_charass,
+        source_key_field, source_number_field, source_name_field,
+        source_desc_field, source_joins,       source_widget,
+        source_key_param, source_uiform_name,  source_create_priv
+      ) values (NULL, 'System',
+         pType,   pFull,   pTable,
+         pDocAss, pCharAss,
+         pKey,    pNumber, pName,
+         pDesc,   pJoin,   pWidget,
+         pParam,  pUi,     pPriv)
+      returning source_id into _id;
+  elsif _justAddedCols then
     update source
-       set source_enum         = COALESCE(pEnum, 0),
+       set source_enum         = pEnum,
+           source_docass       = pDocAss,
+           source_charass      = pCharass,
            source_table        = pTable,
            source_key_field    = pKey,
            source_number_field = pNumber,
@@ -33,26 +53,9 @@ begin
            source_create_priv  = pPriv
      where source_name = pType
      returning source_id into _id;
-    if FOUND then
-      exit;
-    end if;
-    begin
-      insert into source (source_enum, source_module,
-          source_name,      source_descrip,      source_table,
-          source_key_field, source_number_field, source_name_field,
-          source_desc_field, source_joins,       source_widget,
-          source_key_param, source_uiform_name,  source_create_priv
-        ) values (COALESCE(pEnum, 0), 'System',
-           pType,   pFull,   pTable,
-           pKey,    pNumber, pName,
-           pDesc,   pJoin,   pWidget,
-           pParam,  pUi,     pPriv)
-        returning source_id into _id;
-      exit;
-    exception when unique_violation then
-      -- do nothing, loop to try the update again
-    end;
-  end loop;
+  else
+    RAISE NOTICE 'not creating document type %/%/%', pType, pDocAss, pCharAss;
+  end if;
 
   return _id;
 end;
--- foundation-database/public/trigger_functions/source.sql
+++ foundation-database/public/trigger_functions/source.sql
@@ -6,6 +6,17 @@ BEGIN
     NEW.source_created := CURRENT_TIMESTAMP;
   ELSE
     NEW.source_created := OLD.source_created;
+    NEW.source_enum    := COALESCE(NEW.source_enum, OLD.source_enum);
+  END IF;
+
+  IF COALESCE(NEW.source_module, '') = '' THEN
+    NEW.source_module = 'System';
+  END IF;
+  IF NEW.source_enum IS NULL OR
+     EXISTS(SELECT 1 FROM source
+             WHERE source_enum = NEW.source_enum
+               AND source_id != NEW.source_id) THEN
+    SELECT max(source_enum) + 1 FROM SOURCE INTO NEW.source_enum;
   END IF;
   NEW.source_last_modified := CURRENT_TIMESTAMP;
 
