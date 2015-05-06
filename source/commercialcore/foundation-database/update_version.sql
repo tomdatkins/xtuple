6fba79e3f7ae26ad4d4e63e885c19c7cb2611dab
Gil Moskowitz <gilmoskowitz@acm.org>
2015-05-06 13:12:36 -0400

    bump version

--- source/commercialcore/foundation-database/update_version.sql
+++ source/commercialcore/foundation-database/update_version.sql
@@ -1,1 +1,5 @@
-UPDATE pkghead SET pkghead_version = '4.9.0' WHERE pkghead_name = 'xtcore';
+INSERT INTO pkghead (pkghead_name, pkghead_descrip, pkghead_version, pkghead_developer, pkghead_indev)
+SELECT 'xtcore', 'xTuple Commercial Core package.', '0.0.0', 'xTuple', false
+WHERE NOT EXISTS (select c.pkghead_id from pkghead c where c.pkghead_name = 'xtcore');
+
+UPDATE pkghead SET pkghead_version = '4.9.0RC' WHERE pkghead_name = 'xtcore';
