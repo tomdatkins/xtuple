1528b9a8c3540b4f6aa4e2a2abb45d0fa953dfb8
Gil Moskowitz <gilmoskowitz@acm.org>
2015-05-16 07:45:57 -0400

    fix ci failure - always try to create the schema

--- source/commercialcore/foundation-database/update_version.sql
+++ source/commercialcore/foundation-database/update_version.sql
@@ -1,5 +1,10 @@
-INSERT INTO pkghead (pkghead_name, pkghead_descrip, pkghead_version, pkghead_developer, pkghead_indev)
-SELECT 'xtcore', 'xTuple Commercial Core package.', '0.0.0', 'xTuple', false
-WHERE NOT EXISTS (select c.pkghead_id from pkghead c where c.pkghead_name = 'xtcore');
+-- try creating the schema even during upgrades - we _moved_ stuff. this function is idempotent
+select createpkgschema('xtcore', 'This file is part of the xtcore Package for xTuple ERP, and is Copyright (c) 1999-2015 by OpenMFG LLC, d/b/a xTuple.  It is licensed to you under the xTuple End-User License Agreement ("the EULA"), the full text of which is available at www.xtuple.com/EULA.  While the EULA gives you access to source code and encourages your involvement in the development process, this Package is not free software.  By using this software, you agree to be bound by the terms of the EULA.');
 
-UPDATE pkghead SET pkghead_version = '4.9.0RC' WHERE pkghead_name = 'xtcore';
+UPDATE pkghead SET pkghead_descrip   = 'xTuple Commercial Core package.',
+                   pkghead_developer = 'xTuple',
+                   pkghead_indev     = false,
+                   pkghead_version   = '4.9.0RC'
+ WHERE pkghead_name = 'xtcore';
+
+
