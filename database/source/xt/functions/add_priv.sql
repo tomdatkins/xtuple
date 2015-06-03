375ba7113cc220364be94817a982f6e79a2a0220
Ben Thompson <ben@xtuple.com>
2015-06-03 12:18:28 -0400

    Make add_priv flexible to insert to extension schema pkgpriv table.

--- database/source/xt/functions/add_priv.sql
+++ database/source/xt/functions/add_priv.sql
@@ -1,13 +1,24 @@
 drop function if exists xt.add_priv(text, text, text, text);
 
-create or replace function xt.add_priv(name text, descrip text, module text, groop text) returns boolean volatile as $$
+create or replace function xt.add_priv(name text, descrip text, module text, groop text, schema text) returns boolean volatile as $$
 
-  var sqlCount = "select count(*) as count from public.priv where priv_name = $1;",
-    sqlId = "select nextval('public.priv_priv_id_seq') as sequence;",
-    sqlInsert = "insert into public.priv (priv_id, priv_name, priv_module, priv_descrip) values ($1, $2, $3, $4)",
+  var privTable,
+    privSchema,
+    sqlCount = "select count(*) as count from %1$I.%2$I where priv_name = $1;",
+    sqlInsert = "insert into %1$I.%2$I (priv_name, priv_module, priv_descrip) values ($1, $2, $3)",
     sqlGrantToAdmin = "select xt.grant_role_priv('ADMIN', $1, $2)",
-    count = plv8.execute(sqlCount, [ name ])[0].count,
-    nextId;
+    count;
+
+  if (schema && schema !== "public") {
+    privSchema = schema;
+    privTable = "pkgpriv";
+  } else {
+    privSchema = "public";
+    privTable = "priv";
+  }
+
+  sqlCount = XT.format(sqlCount, [privSchema, privTable]);
+  count = plv8.execute(sqlCount, [ name ])[0].count;
 
   if(count > 0) {
     /* Grant this priv to the 'ADMIN' role by default. */
@@ -16,12 +27,11 @@ create or replace function xt.add_priv(name text, descrip text, module text, gro
     return false;
   }
 
-  nextId = plv8.execute(sqlId)[0].sequence;
-
   /* groop is a placeholder until we add a group column in xt.privinfo */
   /* groop is used to segment the assignment box */
   /* module is the name of the extension. was used to filter the assignment box, but doesn't do that any more */
-  plv8.execute(sqlInsert, [nextId, name, module, descrip]);
+  sqlInsert = XT.format(sqlInsert, [privSchema, privTable]);
+  plv8.execute(sqlInsert, [name, module, descrip]);
 
   /* Grant this priv to the 'ADMIN' role by default. */
   plv8.execute(sqlGrantToAdmin, [module, name]);
