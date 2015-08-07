a8e178db93426b71b50d1828ef3637084a0c8075
bartmaj <brtmaj@gmail.com>
2015-08-07 14:39:43 -0400

    Add privilege allowing to edit/create shared filters

--- foundation-database/public/functions/grantpriv.sql
+++ foundation-database/public/functions/grantpriv.sql
@@ -62,3 +62,36 @@ BEGIN
 END;
 $$ LANGUAGE 'plpgsql';
 
+CREATE OR REPLACE FUNCTION grantPrivToAll(TEXT) RETURNS BOOL AS $$
+-- Copyright (c) 1999-2015 by OpenMFG LLC, d/b/a xTuple. 
+-- See www.xtuple.com/CPAL for the full text of the software license.
+DECLARE
+  pPrivname ALIAS FOR $1;
+  _p RECORD;
+  _test INTEGER;
+
+BEGIN
+	FOR _p IN SELECT usr_username AS pUsername FROM usr
+		LOOP 
+		SELECT usrpriv_id INTO _test
+    		FROM usrpriv
+    		JOIN priv ON (usrpriv_priv_id=priv_id)
+ 			 WHERE ((usrpriv_username=_p.pUsername)
+    		 AND (priv_name=pPrivname) );
+
+  			IF (FOUND) THEN
+    			RETURN FALSE;
+  			END IF;
+
+  			INSERT INTO usrpriv
+			  ( usrpriv_username, usrpriv_priv_id )
+ 			 SELECT _p.pUsername, priv_id
+   			 FROM priv
+  				 WHERE (priv_name=pPrivname);
+
+ 			 NOTIFY "usrprivUpdated";
+	END LOOP;	
+RETURN TRUE;
+
+END;
+$$ LANGUAGE plpgsql;
