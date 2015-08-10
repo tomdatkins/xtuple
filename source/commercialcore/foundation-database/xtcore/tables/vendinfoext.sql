7151441914b2f5085c99f34068216819a515a8ef
jcarlin <jcarlin@xtuple.com>
2015-08-10 15:18:49 +0000

    Revert "move XM.PurchaseVendorRelation orm extension to the xt.vendinfoext table so that it doesnt try to extend before existance of req. table"
    This reverts commit 33d8b61a9998435789109561edd84ec0baffba0b.

--- source/commercialcore/foundation-database/xtcore/tables/vendinfoext.sql
+++ source/commercialcore/foundation-database/xtcore/tables/vendinfoext.sql
@@ -5,33 +5,3 @@ select xt.add_column('vendinfoext','vendinfoext_potype_id', 'integer');
 select xt.add_constraint('vendinfoext', 'vendinfoext_potype_id_fkey', 'foreign key (vendinfoext_potype_id) references xt.potype (potype_id)');
 
 comment on table xt.vendinfoext is 'Vendor extension table';
-
--- Extend the XM.PurchaseVendorRelation ORM, add purchaseType.
-select xt.install_orm(
-	'{
-    "context": "commercialcore",
-    "nameSpace": "XM",
-    "type": "PurchaseVendorRelation",
-    "table": "xt.vendinfoext",
-    "isExtension": true,
-    "comment": "Extended by commercialcore",
-    "relations": [
-      {
-        "column": "vendinfoext_id",
-        "inverse": "id"
-      }
-    ],
-    "properties": [
-      {
-        "name": "purchaseType",
-        "toOne": {
-          "type": "PurchaseType",
-          "column": "vendinfoext_potype_id",
-          "isNested": true,
-          "inverse": "id"
-        }
-      }
-    ],
-    "isSystem": true
-  }'
-);
