c2a55b00edf986435730df5303f3b61ed777b8f3
Dave Anderson (xTuple) <danderson@xtuple.com>
2015-09-17 11:21:29 +1200

    Issue #26190: Only check Trial Balance closed periods where necessary

--- foundation-database/public/trigger_functions/trialbal.sql
+++ foundation-database/public/trigger_functions/trialbal.sql
@@ -2,16 +2,29 @@ CREATE OR REPLACE FUNCTION _trialbalaltertrigger()
   RETURNS trigger AS $$
 -- Copyright (c) 1999-2015 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
+DECLARE
+  _accntid INTEGER[];
 BEGIN
   IF(TG_OP='DELETE') THEN
     RAISE EXCEPTION 'You may not delete Trial Balance Transactions.';
   END IF;
 
-  /* bug 26190 vs bug 25392 - uncomment this when the fre stops updating trial balances
-  IF (SELECT period_closed FROM period WHERE period_id=NEW.trialbal_period_id) THEN
-    RAISE EXCEPTION 'You may not alter Trial Balance records in a closed Period.';
-  END IF;
-  */
+  IF (coalesce(fetchMetricValue('GLCompanySize'),0) = 0) THEN
+  --  Retained Earnings account
+      _accntid := fetchmetricvalue('YearEndEquityAccount');
+  ELSE
+  --  Multi-company Retained Earnings setup
+      _accntid := (SELECT array_agg(company_yearend_accnt_id)
+                  FROM company);
+  END IF;    
+
+  If (NEW.trialbal_accnt_id = ANY(_accntid) OR (NEW.trialbal_beginning = 0.00 AND NEW.trialbal_ending = 0.00)) THEN 
+    --  Dont check new accounts or Retained Earnings account 
+  ELSE
+    IF (SELECT period_closed FROM period WHERE period_id=NEW.trialbal_period_id) THEN
+      RAISE EXCEPTION 'You may not alter Trial Balance records in a closed Period.';
+    END IF;
+  END IF;  
   
   RETURN NEW;
 END;
