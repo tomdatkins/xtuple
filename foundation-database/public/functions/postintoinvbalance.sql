748857f71a92245d43668074111e9962bf112dfd
garyhgohoos <gary@xtuple.com>
2015-11-30 11:44:55 -0500

    Issue #26969:prevent avg costed neg balance by period

--- foundation-database/public/functions/postintoinvbalance.sql
+++ foundation-database/public/functions/postintoinvbalance.sql
@@ -1,9 +1,8 @@
 
-CREATE OR REPLACE FUNCTION postIntoInvBalance(INTEGER) RETURNS BOOLEAN AS $$
+CREATE OR REPLACE FUNCTION postIntoInvBalance(pInvhistId INTEGER) RETURNS BOOLEAN AS $$
 -- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  pInvhistId ALIAS FOR $1;
   _invbalid INTEGER;
   _r RECORD;
   _count INTEGER;
@@ -30,7 +29,7 @@ BEGIN
 --  If we can post into a Inv Balance, do so
   IF ( _count > 0 ) THEN
 
---  Validate
+--  Validate period
     IF (_r.period_id IS NULL) THEN
       RAISE EXCEPTION 'No accounting period exists for invhist_id %, transaction date %.  Transaction can not be posted.', _r.invhist_id, formatDate(_r.invhist_transdate);
     END IF;
@@ -147,6 +146,16 @@ BEGIN
           END,
           true );
     END IF;
+
+--  Validate negative balance for average costed item
+    IF EXISTS(SELECT invbal_id
+              FROM invbal JOIN itemsite ON (itemsite_id=invbal_itemsite_id)
+              WHERE (invbal_id=_invbalid)
+                AND (invbal_qoh_ending < 0.0)
+                AND (itemsite_costmethod='A')) THEN
+      RAISE EXCEPTION 'Average costed Item with negative balance for invhist_id %, transaction date %.  Transaction can not be posted.', _r.invhist_id, formatDate(_r.invhist_transdate);
+    END IF;
+
   ELSE
     RETURN FALSE;
   END IF;
@@ -154,5 +163,5 @@ BEGIN
   RETURN TRUE;
 
 END;
-$$ LANGUAGE 'plpgsql';
+$$ LANGUAGE plpgsql;
 
