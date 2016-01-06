9ee03a2f2fa980d0154febf83718ac079c033813
Dave Anderson (xTuple) <danderson@xtuple.com>
2016-01-07 10:39:03 +1300

    Issue #27205 Trial Balance check - additional scenario found

--- foundation-database/public/trigger_functions/trialbal.sql
+++ foundation-database/public/trigger_functions/trialbal.sql
@@ -22,8 +22,9 @@ BEGIN
       _accntid := (SELECT array_agg(company_yearend_accnt_id) FROM company);
     END IF;
 
-    If (NEW.trialbal_accnt_id = ANY(_accntid) OR (NEW.trialbal_beginning = 0.00 AND NEW.trialbal_ending = 0.00)) THEN
-    --  Dont check new accounts or Retained Earnings account 
+    If (NEW.trialbal_accnt_id = ANY(_accntid) OR (NEW.trialbal_beginning = 0.00 AND NEW.trialbal_ending = 0.00)
+         OR (NEW.trialbal_beginning = NEW.trialbal_ending)) THEN
+    --  Dont check new accounts, accounts that have not changed value, or Retained Earnings account 
     ELSE
       IF (SELECT period_closed FROM period WHERE period_id=NEW.trialbal_period_id) THEN
         RAISE EXCEPTION 'You may not alter Trial Balance records in a closed Period.';
