efa3b4f529138d3fc1cb2f155fbfe6abcc44f1d7
garyhgohoos <gary@xtuple.com>
2016-01-13 10:21:17 -0500

    Issue #26687:add foreign key for coitem_qtyreserved_uom_d

--- foundation-database/public/tables/coitem.sql
+++ foundation-database/public/tables/coitem.sql
@@ -1,3 +1,5 @@
-SELECT xt.add_column('coitem', 'coitem_qtyreserved_uom_id', 'INTEGER', NULL, 'public');
+select xt.add_column('coitem', 'coitem_qtyreserved_uom_id', 'INTEGER', NULL, 'public');
+
+select xt.add_constraint('coitem','coitem_coitem_qtyreserved_uom_id_fkey', 'foreign key (coitem_qtyreserved_uom_id) references uom(uom_id)', 'public');
 
 comment on column public.coitem.coitem_qtyreserved_uom_id is 'UOM of qtyreserved (same as Item Inv UOM).';
