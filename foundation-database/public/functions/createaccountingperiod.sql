26349bc0b3deb803bd04cb2417af5d4c0b9758d3
Dave Anderson (xTuple) <danderson@xtuple.com>
2016-01-22 09:27:58 +1300

    Issue #27205 Additional trial balance checks.  Resolve ordering issues

--- foundation-database/public/functions/createaccountingperiod.sql
+++ foundation-database/public/functions/createaccountingperiod.sql
@@ -1,6 +1,6 @@
 
 CREATE OR REPLACE FUNCTION createAccountingPeriod(DATE, DATE) RETURNS INTEGER AS $$
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+-- Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   pStartDate ALIAS FOR $1;
@@ -14,7 +14,7 @@ END;
 $$ LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE FUNCTION createaccountingperiod(DATE, DATE, INTEGER, INTEGER) RETURNS INTEGER AS $$
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+-- Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   pStartDate ALIAS FOR $1;
@@ -96,6 +96,7 @@ BEGIN
     SELECT DISTINCT ON (trialbal_accnt_id) trialbal_id
     FROM trialbal
     JOIN period ON (period_id=trialbal_period_id)
+    WHERE period_id = _periodid
     ORDER BY trialbal_accnt_id, period_end DESC
   LOOP
     PERFORM forwardupdatetrialbalance(_r.trialbal_id);
