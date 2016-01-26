e319f0c7dcd12743029c81ff3f4b55dbcf746c33
jcarlin <jcarlin@xtuple.com>
2016-01-26 20:12:07 +0000

    enterreceiptdetail function, recvdetail table change

--- source/inventory/database/source/xt/tables/recvdetail.sql
+++ source/inventory/database/source/xt/tables/recvdetail.sql
@@ -1,13 +1,14 @@
 select xt.create_table('recvdetail');
 
 select xt.add_column('recvdetail','recvdetail_id', 'integer', 'primary key');
-select xt.add_column('recvdetail','recvext_poitem_id', 'integer', );
-select xt.add_column('recvdetail','recvext_qty', 'numeric');
-select xt.add_column('recvdetail','recvext_location_id', 'integer');
-select xt.add_column('recvdetail','recvext_lot', 'text');
-select xt.add_column('recvdetail','recvext_expiration', 'date');
+select xt.add_column('recvdetail','recvdetail_ordertype', 'text');
+select xt.add_column('recvdetail','recvdetail_orderhead_id', 'integer');
+select xt.add_column('recvdetail','recvdetail_orderitem_id', 'integer');
+select xt.add_column('recvdetail','recvdetail_qty', 'numeric');
+select xt.add_column('recvdetail','recvdetail_location_id', 'integer');
+select xt.add_column('recvdetail','recvdetail_lot', 'text');
+select xt.add_column('recvdetail','recvdetail_expiration', 'date');
 
-select xt.add_constraint('recvdetail', 'recvdetail_poitem_id_fkey', 'foreign key (recvdetail_poitem_id) references poitem (poitem_id)');
 select xt.add_constraint('recvdetail', 'recvdetail_location_id_fkey', 'foreign key (recvdetail_location_id) references location (location_id)');
 
 comment on table xt.recvdetail is 'XTMI - receipt distribution detail';
