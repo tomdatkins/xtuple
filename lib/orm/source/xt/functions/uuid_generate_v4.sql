a171bc3bcf43b02a12b7ea73a7b2ca3b31e2fe41
Ben Thompson <ben@xtuple.com>
2016-02-10 14:22:07 -0500

    Convert xt.uuid_generate_v4() to plpgsql.

--- lib/orm/source/xt/functions/uuid_generate_v4.sql
+++ lib/orm/source/xt/functions/uuid_generate_v4.sql
@@ -4,12 +4,12 @@
     @returns {text}
 */
 create or replace function xt.uuid_generate_v4() returns uuid as $$
-
+BEGIN
   /*
    * Uses the `uuid-ossp` Postgres extension to create a UUID.
    * @See: http://www.postgresql.org/docs/9.1/static/uuid-ossp.html
    */
 
-  return uuid_generate_v4();
-
-$$ language plv8;
+  RETURN uuid_generate_v4();
+END;
+$$ language plpgsql VOLATILE;
