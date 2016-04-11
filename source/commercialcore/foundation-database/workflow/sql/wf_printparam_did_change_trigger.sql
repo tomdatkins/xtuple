80d948ea7b8c864fd6b0bb08a2b8a022f8cdb094
BC Wilson <bc@xtuple.com>
2016-04-11 16:10:33 -0400

    fixes for PR#764

--- source/commercialcore/foundation-database/workflow/sql/wf_printparam_did_change_trigger.sql
+++ source/commercialcore/foundation-database/workflow/sql/wf_printparam_did_change_trigger.sql
@@ -5,6 +5,15 @@ CREATE OR REPLACE FUNCTION workflow.send_printparam_to_batchparam()
 $BODY$
 
 return (function () { 
+   /* MAKE SURE XTBATCH IS PRESENT AND ENABLED */
+   var xtbatchSQL = "SELECT packageIsEnabled(pkghead_name) AS enabled FROM pkghead WHERE pkghead_name = 'xtbatch'";
+   var xtbatch = plv8.execute(xtbatchSQL);
+   if (!xtbatch[0].enabled)
+   {
+     plv8.elog("xtbatch schema is not installed or is inactive");
+     return;
+   }  
+   
    plv8.elog(WARNING, "send_printparam_to_batchparam: NEW.wf_printparam_parent_uuid is " + NEW.wf_printparam_parent_uuid);
    var batchid = -1;
    var skip_update = false;
@@ -19,79 +28,90 @@ return (function () {
           + " WHERE batchparam_value = $1::text ORDER BY batchparam_batch_id ASC LIMIT 1";   
       var checkbatch = plv8.execute(checkbatchSQL, [NEW.wf_printparam_parent_uuid]);
            
-      checkbatch.map(function (batchitems) 
+      if (checkbatch.length > 0 )
       {
-         plv8.elog(WARNING, "batch found: enter batchitems map");
-         batchid = batchitems.batchparam_batch_id;       
-      })
+         batchid = checkbatch[0].batchparam_batch_id;       
+         plv8.elog(WARNING, "batch found: " + batchid);
+      }
+      
       /* if not, insert new batch */
       if (batchid < 0) 
       {   
          plv8.elog(WARNING, "batch not found, insert new");
          
-         /* get the report name */
-         var getreportSQL = "SELECT report.wf_printparam_value AS report    "
-                + "       ,printer.wf_printparam_value         AS printer   "
-                + "       ,fromemail.wf_printparam_value       AS fromemail " 
-                + "       ,toemail.wf_printparam_value         AS toemail   "
-                + " FROM   workflow.wf_printparam report                    "
-                + "       ,workflow.wf_printparam printer                   "
-                + "       ,workflow.wf_printparam fromemail                 "
-                + "       ,workflow.wf_printparam toemail                   "
-                + " WHERE (   report.wf_printparam_name = 'name'            "
-                + "        AND    report.wf_printparam_parent_uuid = $1)    "
-                + "   AND (  printer.wf_printparam_name = 'reportPrinter'   "
-                + "        AND   printer.wf_printparam_parent_uuid = $1)    "
-                + "   AND (fromemail.wf_printparam_name = 'fromemail'       "
-                + "        AND fromemail.wf_printparam_parent_uuid = $1)    "
-                + "   AND (  toemail.wf_printparam_name = 'toemail'         "
-                + "        AND   toemail.wf_printparam_parent_uuid = $1)    ";
+         /* get the params */
+         var param = new Object;
+         var getreportSQL = "SELECT wf_printparam_name, "
+           + " wf_printparam_value, wf_printparam_parent_uuid "
+           + " FROM workflow.wf_printparam "
+           + " WHERE wf_printparam_name IN "
+           + " ('head_type','billing','invoice','name','reportPrinter','fromemail','toemail') "
+           + " AND wf_printparam_parent_uuid = $1 ";
          var reportparam = plv8.execute(getreportSQL, NEW.wf_printparam_parent_uuid);
-         
-         reportparam.map(function (reportitems)
-         {                
-            var getordernumSQL = "SELECT cohead_number FROM cohead "
+         reportparam.forEach(function (elem) {
+           param[elem.wf_printparam_name] = elem.wf_printparam_value
+           plv8.elog(WARNING, elem.wf_printparam_name + ": " + elem.wf_printparam_value);
+         });
+               
+         if(param.head_type == 'SO')
+         {
+              var getordernumSQL = "SELECT cohead_number AS ordernum FROM cohead "
                 + " WHERE cohead.obj_uuid = ( "
                 + " SELECT wf_parent_uuid FROM xt.wf "
                 + " WHERE wf.obj_uuid = $1 ) ";
-            var getordernum = plv8.execute(getordernumSQL, [NEW.wf_printparam_parent_uuid]);
+         } else if(param.head_type == 'PO')
+         {
+              var getordernumSQL = "SELECT pohead_number AS ordernum FROM pohead "
+                + " WHERE pohead.obj_uuid = ( "
+                + " SELECT wf_parent_uuid FROM xt.wf "
+                + " WHERE wf.obj_uuid = $1 ) ";
+         } else if(param.head_type == 'TO')
+         {
+              var getordernumSQL = "SELECT tohead_number AS ordernum FROM tohead "
+                + " WHERE tohead.obj_uuid = ( "
+                + " SELECT wf_parent_uuid FROM xt.wf "
+                + " WHERE wf.obj_uuid = $1 ) ";            
+         }
+         var getordernum = plv8.execute(getordernumSQL, [NEW.wf_printparam_parent_uuid]);
             
-            var ordernum = getordernum[0].cohead_number;
-            plv8.elog(WARNING, "order number is " + ordernum);
-            
-            var insertbatchSQL = "INSERT INTO xtbatch.batch "
+         var ordernum = getordernum[0].ordernum;
+         plv8.elog(WARNING, param.head_type + " number is " + ordernum);
+          
+         /* insert the params */   
+         var insertbatchSQL = "INSERT INTO xtbatch.batch "
                 + " (batch_action, batch_parameter, "
                 + "  batch_user, batch_email, "
                 + "  batch_subject, batch_responsebody, "
                 + "  batch_fromemail, batch_replyto, "
                 + "  batch_submitted, batch_scheduled) "
                 + " VALUES "
-                + " ('RunReport', $1, " // report name is parameter
-                + "   current_user, $4, " // TODO: change this address after testing
+                + " ('RunReport', $1, "   
+                + "   current_user, $4, " 
                 + "   $1||' Printed for Order #'||$2, "
-                + "   $1||' for order # '||$2||' sent to printer '||$3||' on '|| localtimestamp, "
+                + "   $1||' for '||$6||' # '||$2||' sent to printer '||$3||' on '|| localtimestamp, "
                 + "   $5, $5, "
                 + "   localtimestamp, localtimestamp) "
                 + " RETURNING batch_id, batch_parameter" ;
-            var insertbatch = plv8.execute(insertbatchSQL, reportitems.report, ordernum,
-                reportitems.printer, reportitems.toemail, reportitems.fromemail);
-            batchid = insertbatch[0].batch_id;
-            plv8.elog(WARNING, "batch_id is " + batchid + " and batch_parameter is " 
+         var insertbatch = plv8.execute(insertbatchSQL, [param.name, ordernum,
+                param.reportPrinter, param.toemail, param.fromemail, param.head_type]);
+         batchid = insertbatch[0].batch_id;
+         plv8.elog(WARNING, "batch_id is " + batchid + " and batch_parameter is " 
                                    + insertbatch[0].batch_parameter);
             
-            /* insert first batchparam with uuid */
-            var insertfirstbatchparamSQL = "INSERT INTO xtbatch.batchparam "
+         /* insert first batchparam with uuid */
+         var insertfirstbatchparamSQL = "INSERT INTO xtbatch.batchparam "
                 + " (batchparam_batch_id, batchparam_order, "
                 + "  batchparam_name, batchparam_value, batchparam_type) "
                 + " VALUES ( $1, 0, "
                 + "  'wf_uuid', $2::text, 'text' ) returning batchparam_id"; 
-            plv8.execute(insertfirstbatchparamSQL, [batchid, NEW.wf_printparam_parent_uuid]);
-         });
-      }            
-      plv8.elog(WARNING, "batchid is " + batchid);
-        
+         plv8.execute(insertfirstbatchparamSQL, [batchid, NEW.wf_printparam_parent_uuid]);
+            
+         /* TODO: add select for billing and create invoice steps */
+
+
+      }                    
       /* check for batchparam */ 
-      plv8.elog(WARNING, "finding batchid " + batchid + ", order " + NEW.wf_printparam_order);
+      plv8.elog(WARNING, "finding batchparam for batchid " + batchid + ", order " + NEW.wf_printparam_order);
 
       var checkbatchparamSQL = "SELECT batchparam_id FROM xtbatch.batchparam "
             + " WHERE batchparam_batch_id = $1 "
@@ -101,7 +121,7 @@ return (function () {
       checkbatchparam.map(function (bpitems) 
       { 
          /* Update batchparam value*/
-         plv8.elog(WARNING, "updating batchid " + batchid + ", order " + NEW.wf_printparam_order);
+         plv8.elog(WARNING, "updating batchparam for batchid " + batchid + ", order " + NEW.wf_printparam_order);
 
          var updatebpSQL = "UPDATE xtbatch.batchparam "
                   + " SET batchparam_value = $1 "
@@ -114,7 +134,7 @@ return (function () {
       if (!skip_update)  
       {
          /* Insert printparam into batchparam */
-         plv8.elog(WARNING, "inserting batchid " + batchid + ", order " + NEW.wf_printparam_order);
+         plv8.elog(WARNING, "inserting batchparam for batchid " + batchid + ", order " + NEW.wf_printparam_order);
 
          var insertbpSQL = "INSERT INTO xtbatch.batchparam "
                + " (batchparam_batch_id, batchparam_order, "
