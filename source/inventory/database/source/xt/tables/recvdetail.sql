b1a445cc0519282b46f38be8c42bf1d5963806ec
vagrant <vagrant@xtuple-server>
2016-04-12 16:26:58 +0000

    recvdetail_id primary key constraint

--- source/inventory/database/source/xt/tables/recvdetail.sql
+++ source/inventory/database/source/xt/tables/recvdetail.sql
@@ -1,6 +1,6 @@
 select xt.create_table('recvdetail');
 
-select xt.add_column('recvdetail','recvdetail_id', 'integer', 'primary key');
+select xt.add_column('recvdetail','recvdetail_id', 'serial', 'primary key');
 select xt.add_column('recvdetail','recvdetail_order_type', 'text');
 select xt.add_column('recvdetail','recvdetail_orderhead_id', 'integer');
 select xt.add_column('recvdetail','recvdetail_orderitem_id', 'integer');
@@ -12,6 +12,7 @@ select xt.add_column('recvdetail','recvdetail_warranty', 'date');
 select xt.add_column('recvdetail','recvdetail_posted', 'boolean');
 select xt.add_column('recvdetail','recvdetail_itemsite_id', 'integer');
 
+select xt.add_constraint('recvdetail', 'recvdetail_pkey', 'primary key (recvdetail_id)');
 select xt.add_constraint('recvdetail', 'recvdetail_location_id_fkey', 'foreign key (recvdetail_location_id) references location (location_id)');
 select xt.add_constraint('recvdetail', 'recvdetail_itemsite_id_fkey', 'foreign key (recvdetail_itemsite_id) references itemsite (itemsite_id)');
 
