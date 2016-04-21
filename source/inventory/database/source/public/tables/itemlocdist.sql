21caa5c2fe4ddff571b40559d586909ae1fe4edc
Julian Carlin <jcarlin@xtuple.com>
2016-04-21 21:48:58 +0000

    replace xt.recvdetail with xt.distdetail. Remove previous itemlocdist trigger work

--- source/inventory/database/source/public/tables/itemlocdist.sql
+++
@@ -1,3 +0,0 @@
-DROP TRIGGER IF EXISTS itemlocdist_before_trigger ON itemlocdist;
-CREATE TRIGGER itemlocdist_before_trigger BEFORE DELETE ON itemlocdist FOR EACH ROW
-  EXECUTE PROCEDURE xt.itemlocdist_save_distribution_detail();
--- source/inventory/database/source/xt/functions/distributedetail.sql
+++ source/inventory/database/source/xt/functions/distributedetail.sql
@@ -20,20 +20,19 @@ begin
    See www.xtuple.com/CPAL for the full text of the software license. */
   _qtyToRecv := pQty;
 
-  SELECT sum(recvdetail_qty) AS sumqty, count(*) AS count INTO _existingQtyRecvd, _detRecCount
-  FROM xt.recvdetail 
-  WHERE recvdetail_order_type = pOrderType
-    AND recvdetail_orderhead_id = pOrderId
-    AND recvdetail_orderitem_id = pOrderItemId
-    AND recvdetail_lot = pLot
-    AND COALESCE(recvdetail_location_id, -1) = COALESCE(pLocId, -1)
-    AND recvdetail_itemsite_id = pItemsiteId
-    AND NOT recvdetail_posted
-  GROUP BY recvdetail_orderhead_id, recvdetail_orderitem_id;
+  SELECT sum(distdetail_qty) AS sumqty, count(*) AS count INTO _existingQtyRecvd, _detRecCount
+  FROM xt.distdetail 
+  WHERE distdetail_order_type = pOrderType
+    AND distdetail_orderhead_id = pOrderId
+    AND distdetail_orderitem_id = pOrderItemId
+    AND distdetail_lot = pLot
+    AND COALESCE(distdetail_location_id, -1) = COALESCE(pLocId, -1)
+    AND distdetail_itemsite_id = pItemsiteId
+  GROUP BY distdetail_orderhead_id, distdetail_orderitem_id;
 
   IF FOUND THEN
     IF (_detRecCount > 1) THEN 
-    	RAISE EXCEPTION 'Duplicate recvdetail records found! Can not Enter Receipt.
+    	RAISE EXCEPTION 'Duplicate distdetail records found! Can not Enter Receipt.
     		[xtuple: xt.distributedetail, -1]';
     ELSE
       _qtyToRecv := _existingQtyRecvd + pQty;
@@ -42,40 +41,37 @@ begin
         RAISE EXCEPTION 'Can not receive negative qty! [xtuple: xt.distributedetail, -2]'; 
       ELSEIF (_qtyToRecv = 0) THEN 
         DELETE 
-        FROM xt.recvdetail 
-        WHERE recvdetail_order_type = pOrderType
-          AND recvdetail_orderhead_id = pOrderId
-          AND recvdetail_orderitem_id = pOrderItemId
-          AND recvdetail_lot = pLot
-          AND COALESCE(recvdetail_location_id, -1) = COALESCE(pLocId, -1)
-          AND NOT recvdetail_posted;
+        FROM xt.distdetail 
+        WHERE distdetail_order_type = pOrderType
+          AND distdetail_orderhead_id = pOrderId
+          AND distdetail_orderitem_id = pOrderItemId
+          AND distdetail_lot = pLot
+          AND COALESCE(distdetail_location_id, -1) = COALESCE(pLocId, -1);
       ELSE
-        UPDATE xt.recvdetail
-        SET recvdetail_qty = _qtyToRecv
-        WHERE recvdetail_order_type = pOrderType
-          AND recvdetail_orderhead_id = pOrderId
-          AND recvdetail_orderitem_id = pOrderItemId
-          AND recvdetail_lot = pLot
-          AND COALESCE(recvdetail_location_id, -1) = COALESCE(pLocId, -1)
-          AND NOT recvdetail_posted;
+        UPDATE xt.distdetail
+        SET distdetail_qty = _qtyToRecv
+        WHERE distdetail_order_type = pOrderType
+          AND distdetail_orderhead_id = pOrderId
+          AND distdetail_orderitem_id = pOrderItemId
+          AND distdetail_lot = pLot
+          AND COALESCE(distdetail_location_id, -1) = COALESCE(pLocId, -1);
       END IF;
     END IF;
   ELSEIF NOT FOUND THEN 
-    INSERT INTO xt.recvdetail (recvdetail_order_type, recvdetail_orderhead_id,
-      recvdetail_orderitem_id, recvdetail_qty, recvdetail_location_id, recvdetail_lot,
-      recvdetail_expiration, recvdetail_warranty, recvdetail_posted, recvdetail_itemsite_id)
+    INSERT INTO xt.distdetail (distdetail_order_type, distdetail_orderhead_id,
+      distdetail_orderitem_id, distdetail_qty, distdetail_location_id, distdetail_lot,
+      distdetail_expiration, distdetail_warranty, distdetail_itemsite_id)
     VALUES (pOrderType, pOrderId, pOrderItemId, _qtyToRecv, pLocId, pLot, pExpDate::DATE,
-      pWarrDate::DATE, false, pItemsiteId);
+      pWarrDate::DATE, pItemsiteId);
   END IF;
 
   IF (pOrderType = 'PO') THEN
     -- Enter Receipt overrides prev. receipts with new qty so get the sum qty from the detail
-    SELECT sum(recvdetail_qty) INTO _qtyToRecv
-    FROM xt.recvdetail 
-    WHERE recvdetail_orderhead_id = pOrderId
-      AND recvdetail_orderitem_id = pOrderItemId
-      AND recvdetail_posted = FALSE
-    GROUP BY recvdetail_orderhead_id;
+    SELECT sum(distdetail_qty) INTO _qtyToRecv
+    FROM xt.distdetail 
+    WHERE distdetail_orderhead_id = pOrderId
+      AND distdetail_orderitem_id = pOrderItemId
+    GROUP BY distdetail_orderhead_id;
 
     -- enterreceipt(orderType, orderItemId, qty, freight, notes, currId, recvDate, recvCost)
     SELECT enterreceipt(pOrderType, pOrderItemId, _qtyToRecv, 0, NULL::TEXT, NULL::INTEGER,
