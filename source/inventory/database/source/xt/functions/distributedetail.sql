7c9cbec429285d87b060d426ef5a03be70fac394
Julian Carlin <jcarlin@xtuple.com>
2016-04-26 21:23:03 +0000

    simplify xt.distributedetail function

--- source/inventory/database/source/xt/functions/distributedetail.sql
+++ source/inventory/database/source/xt/functions/distributedetail.sql
@@ -7,77 +7,46 @@ create or replace function xt.distributedetail(pOrderType	text, pOrderId integer
 RETURNS integer AS $$
 
 declare
-
-  _detRecCount integer;
-  _existingQtyRecvd numeric;
-  _calcQtyToRecv numeric;
   _qtyToRecv numeric;
-  _result integer;
-
 begin
-  _result := 0;
 	/* Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
    See www.xtuple.com/CPAL for the full text of the software license. */
-  _qtyToRecv := pQty;
 
-  SELECT sum(distdetail_qty) AS sumqty, count(*) AS count INTO _existingQtyRecvd, _detRecCount
-  FROM xt.distdetail 
+  UPDATE xt.distdetail
+  SET distdetail_qty = distdetail_qty + pQty
   WHERE distdetail_order_type = pOrderType
     AND distdetail_orderhead_id = pOrderId
     AND distdetail_orderitem_id = pOrderItemId
     AND distdetail_lot = pLot
-    AND COALESCE(distdetail_location_id, -1) = COALESCE(pLocId, -1)
     AND distdetail_itemsite_id = pItemsiteId
-  GROUP BY distdetail_orderhead_id, distdetail_orderitem_id;
+    AND COALESCE(distdetail_location_id, -1) = COALESCE(pLocId, -1)
+  RETURNING distdetail_qty INTO STRICT _qtyToRecv;
 
-  IF FOUND THEN
-    IF (_detRecCount > 1) THEN 
-    	RAISE EXCEPTION 'Duplicate distdetail records found! Can not Enter Receipt.
-    		[xtuple: xt.distributedetail, -1]';
-    ELSE
-      _qtyToRecv := _existingQtyRecvd + pQty;
-      RAISE NOTICE '_qtyToRecv: %', _qtyToRecv;
-      IF (_qtyToRecv < 0) THEN 
-        RAISE EXCEPTION 'Can not receive negative qty! [xtuple: xt.distributedetail, -2]'; 
-      ELSEIF (_qtyToRecv = 0) THEN 
-        DELETE 
-        FROM xt.distdetail 
-        WHERE distdetail_order_type = pOrderType
-          AND distdetail_orderhead_id = pOrderId
-          AND distdetail_orderitem_id = pOrderItemId
-          AND distdetail_lot = pLot
-          AND COALESCE(distdetail_location_id, -1) = COALESCE(pLocId, -1);
-      ELSE
-        UPDATE xt.distdetail
-        SET distdetail_qty = _qtyToRecv
-        WHERE distdetail_order_type = pOrderType
-          AND distdetail_orderhead_id = pOrderId
-          AND distdetail_orderitem_id = pOrderItemId
-          AND distdetail_lot = pLot
-          AND COALESCE(distdetail_location_id, -1) = COALESCE(pLocId, -1);
-      END IF;
-    END IF;
-  ELSEIF NOT FOUND THEN 
-    INSERT INTO xt.distdetail (distdetail_order_type, distdetail_orderhead_id,
-      distdetail_orderitem_id, distdetail_qty, distdetail_location_id, distdetail_lot,
-      distdetail_expiration, distdetail_warranty, distdetail_itemsite_id)
-    VALUES (pOrderType, pOrderId, pOrderItemId, _qtyToRecv, pLocId, pLot, pExpDate::DATE,
-      pWarrDate::DATE, pItemsiteId);
+  IF _qtyToRecv < 0 THEN 
+    RAISE EXCEPTION 'Can not receive negative qty! [xtuple: xt.distributedetail, -1]';
+  ELSEIF _qtyToRecv = 0 THEN
+    DELETE 
+      FROM xt.distdetail 
+      WHERE distdetail_order_type = pOrderType
+        AND distdetail_orderhead_id = pOrderId
+        AND distdetail_orderitem_id = pOrderItemId
+        AND distdetail_lot = pLot
+        AND distdetail_itemsite_id = pItemsiteId
+        AND COALESCE(distdetail_location_id, -1) = COALESCE(pLocId, -1);
   END IF;
 
-  IF (pOrderType = 'PO') THEN
-    -- Enter Receipt overrides prev. receipts with new qty so get the sum qty from the detail
-    SELECT sum(distdetail_qty) INTO _qtyToRecv
-    FROM xt.distdetail 
-    WHERE distdetail_orderhead_id = pOrderId
-      AND distdetail_orderitem_id = pOrderItemId
-    GROUP BY distdetail_orderhead_id;
+  RETURN 0;
 
-    -- enterreceipt(orderType, orderItemId, qty, freight, notes, currId, recvDate, recvCost)
-    SELECT enterreceipt(pOrderType, pOrderItemId, _qtyToRecv, 0, NULL::TEXT, NULL::INTEGER,
-      current_date, NULL::NUMERIC) INTO _result;
-  END IF;
-
-  RETURN _result;
+  EXCEPTION
+    WHEN TOO_MANY_ROWS THEN -- If >1 rows, throw error
+      RAISE EXCEPTION 'Duplicate distdetail records found! Can not Enter Receipt.
+        [xtuple: xt.distributedetail, -2]';
+    WHEN NO_DATA_FOUND THEN
+      INSERT INTO xt.distdetail (distdetail_order_type, distdetail_orderhead_id,
+        distdetail_orderitem_id, distdetail_qty, distdetail_location_id, distdetail_lot,
+        distdetail_expiration, distdetail_warranty, distdetail_itemsite_id)
+      VALUES (pOrderType, pOrderId, pOrderItemId, pQty, pLocId, pLot, pExpDate::DATE,
+        pWarrDate::DATE, pItemsiteId);
+      RETURN 1; 
 end
 $$ language 'plpgsql' volatile;
