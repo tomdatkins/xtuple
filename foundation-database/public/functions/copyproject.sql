daa153967cff6f9fe4d330bd0e12ff69281f125a
Dave Anderson (xTuple) <danderson@xtuple.com>
2016-05-30 13:14:11 +1200

    Issue #27716: Copy Project function - include TE billing in copy

--- foundation-database/public/functions/copyproject.sql
+++ foundation-database/public/functions/copyproject.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE FUNCTION copyproject(integer, text, text, date)
-  RETURNS integer AS
-$BODY$
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+  RETURNS integer AS $$
+-- Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   pPrjId ALIAS FOR $1;
@@ -9,8 +8,10 @@ DECLARE
   pPrjName ALIAS FOR $3;
   pDueDate ALIAS FOR $4;
   _prjid INTEGER;
+  _prjtasks RECORD;
   _offset INTEGER;
-
+  _newTaskid INTEGER;
+  _teExists BOOLEAN;
 BEGIN
 
   IF (COALESCE(pPrjNumber, '') = '') THEN
@@ -37,6 +38,8 @@ BEGIN
    FROM prj
    WHERE (prj_id=pPrjId);
 
+  _teExists := EXISTS(SELECT 1 FROM pkghead WHERE pkghead_name='te');
+
   SELECT NEXTVAL('prj_prj_id_seq') INTO _prjid;
 
   INSERT INTO prj
@@ -58,25 +61,59 @@ BEGIN
   FROM prj
   WHERE (prj_id=pPrjId);
 
-  INSERT INTO prjtask
-  ( prjtask_number, prjtask_name, prjtask_descrip,
-    prjtask_prj_id, prjtask_anyuser, prjtask_status,
-    prjtask_hours_budget, prjtask_hours_actual,
-    prjtask_exp_budget, prjtask_exp_actual,
-    prjtask_owner_username, prjtask_start_date,
-    prjtask_due_date, prjtask_assigned_date,
-    prjtask_completed_date, prjtask_username )
-  SELECT prjtask_number, prjtask_name, prjtask_descrip,
-         _prjid, prjtask_anyuser, 'P',
-         prjtask_hours_budget, 0.0,
-         prjtask_exp_budget, 0.0,
-         prjtask_owner_username, NULL,
-         (prjtask_due_date + COALESCE(_offset, 0)),
-         CASE WHEN (prjtask_username IS NULL) THEN NULL ELSE CURRENT_DATE END,
-         NULL, prjtask_username
-  FROM prjtask
-  WHERE (prjtask_prj_id=pPrjId);
+  IF (_teExists) THEN
+  -- Also insert TE billing information (if pkg exists)
+    INSERT INTO te.teprj
+      (teprj_prj_id, teprj_cust_id,
+       teprj_rate, teprj_curr_id)
+     SELECT _prjid, teprj_cust_id,
+       teprj_rate, teprj_curr_id
+     FROM te.teprj
+     WHERE (teprj_prj_id = pPrjId);
+  END IF;
 
+  FOR _prjtasks IN
+      SELECT prjtask_id, prjtask_number, prjtask_name, prjtask_descrip,
+         prjtask_prj_id, prjtask_anyuser, 'P',
+         prjtask_hours_budget, prjtask_exp_budget, prjtask_owner_username,
+         prjtask_due_date, prjtask_username
+      FROM prjtask
+      WHERE (prjtask_prj_id=pPrjId)
+  LOOP
+     
+    INSERT INTO prjtask
+    ( prjtask_number, prjtask_name, prjtask_descrip,
+      prjtask_prj_id, prjtask_anyuser, prjtask_status,
+      prjtask_hours_budget, prjtask_hours_actual,
+      prjtask_exp_budget, prjtask_exp_actual,
+      prjtask_owner_username, prjtask_start_date,
+      prjtask_due_date, prjtask_assigned_date,
+      prjtask_completed_date, prjtask_username )
+    VALUES (_prjtasks.prjtask_number, _prjtasks.prjtask_name, _prjtasks.prjtask_descrip,
+            _prjid, _prjtasks.prjtask_anyuser, 'P',
+            _prjtasks.prjtask_hours_budget, 0.0,
+            _prjtasks.prjtask_exp_budget, 0.0,
+            _prjtasks.prjtask_owner_username, NULL,
+            (_prjtasks.prjtask_due_date + COALESCE(_offset, 0)),
+            CASE WHEN (_prjtasks.prjtask_username IS NULL) THEN NULL ELSE CURRENT_DATE END,
+            NULL, _prjtasks.prjtask_username)
+    RETURNING prjtask_id INTO _newTaskid;
+
+    IF (_teExists) THEN
+    -- Also insert TE billing information (if pkg exists)
+      INSERT INTO te.teprjtask
+        ( teprjtask_cust_id, teprjtask_rate,
+          teprjtask_item_id, teprjtask_prjtask_id,
+          teprjtask_curr_id)
+      SELECT teprjtask_cust_id, teprjtask_rate,       
+             teprjtask_item_id, _newTaskid,
+             teprjtask_curr_id
+      FROM te.teprjtask
+      WHERE (teprjtask_prjtask_id=_prjtasks.prjtask_id);
+      
+    END IF; 
+  END LOOP;          
+   
   INSERT INTO docass
   ( docass_source_id, docass_source_type,
     docass_target_id, docass_target_type,
@@ -91,9 +128,7 @@ BEGIN
   RETURN _prjid;
 
 END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION copyproject(integer, text, text, date)
-  OWNER TO admin;
+$$ LANGUAGE plpgsql;
+
+ALTER FUNCTION copyproject(integer, text, text, date) OWNER TO admin;
 
