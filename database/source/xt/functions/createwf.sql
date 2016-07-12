6161df182c7a859102f5fe94e2b45b1b88a73123
BC Wilson <bc@xtuple.com>
2016-07-12 17:18:18 -0400

    issue #28209 added an update trigger for cohead table so when sale type is changed it will update the workflow

--- database/source/xt/functions/createwf.sql
+++ database/source/xt/functions/createwf.sql
@@ -1,80 +1,89 @@
-CREATE OR REPLACE FUNCTION xt.createwf(tg_table_name text, tg_table_row record)
+CREATE OR REPLACE FUNCTION xt.createwf(text, record)
   RETURNS record AS
-$BODY$
+$CREATEWF$
 /* Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.
    See www.xm.ple.com/CPAL for the full text of the software license. */
+DECLARE
+tg_table_name ALIAS FOR $1; 
+tg_table_row  ALIAS FOR $2;
+_metric       BOOLEAN   := false;
+_wftypecode   TEXT      := '';
+_source_model TEXT      := '';
+_workflow_class TEXT    := '';
+_item_uuid    TEXT      := '';
+_parent_id    INTEGER   := 0;
+_order_id     INTEGER   := 0;
+_poparent     RECORD;
 
-return (function () {
+BEGIN
 
-    if (plv8.execute("select fetchmetricbool('TriggerWorkflow') as val;")[0].val) {
-      var parent,
-        parentId,
-        parentIdSql,
-        sourceModSql = "select wftype_src_tblname as srctblname from xt.wftype where wftype_code = $1 ",
-        sourceModel;
+   SELECT fetchmetricbool('TriggerWorkflow') INTO _metric;
+   IF (_metric) THEN
+            
+      -- Get data by type      
+      IF (tg_table_name = 'cohead') THEN
+         _wftypecode := 'SO';
+         _workflow_class  := 'XM.SalesOrderWorkflow';
+         _item_uuid := tg_table_row.obj_uuid;
+         _parent_id := tg_table_row.cohead_saletype_id;
+         _order_id := tg_table_row.cohead_id;
+         
+      ELSE IF (tg_table_name = 'prj') THEN
+         _wftypecode := 'PRJ';
+         _workflow_class  := 'XM.ProjectWorkflow';
+         _item_uuid := tg_table_row.obj_uuid;
+         _parent_id := tg_table_row.prj_prjtype_id;
+         _order_id := tg_table_row.prj_id;
+         
+      ELSE IF (tg_table_name = 'poheadext') THEN
+         _wftypecode := 'PO';
+         _workflow_class  := 'XM.PurchaseOrderWorkflow';
+         SELECT poheadext_potype_id AS parent_id, pohead_id, pohead.obj_uuid AS pohead_uuid INTO _poparent;
+         FROM xt.poheadext JOIN pohead ON poheadext_id = pohead_id WHERE poheadext_id = tg_table_row.poheadext_id;
+         IF (NOT FOUND) THEN
+           RAISE WARNING, "Missing parentId needed to generate workflow!";
+         END IF;
+         _item_uuid    := _poparent.pohead_uuid;
+         _parent_id    := _poparent.pohead_id;
+         _order_id     := _poparent.pohead_id;
 
-      if (tg_table_name === 'cohead') {
-        sourceModel = plv8.execute(sourceModSql, ['SO'])[0].srctblname;
+      ELSE IF (tg_table_name = 'tohead') THEN
+         _wftypecode := 'TO';
+         _workflow_class  := 'XM.TransferOrderWorkflow';
+         _item_uuid := tg_table_row.obj_uuid;
+         SELECT warehous_sitetype_id AS parent_id INTO _parent_id FROM warehous WHERE warehous_id = tg_table_row.tohead_src_warehous_id;
+         IF (NOT FOUND) THEN
+           RAISE WARNING, "Missing parentId needed to generate workflow!";
+         END IF;
+         _order_id     := tg_table_row.tohead_id
 
-        if (!sourceModel) {
-          plv8.elog(WARNING, "Missing sourceModel and/or parentId needed to generate workflow!");
-        }
-        plv8.execute("SELECT xt.workflow_inheritsource($1, $2, $3, $4, $5);",
-          ["xt." + sourceModel, 'XM.SalesOrderWorkflow', tg_table_row.obj_uuid, tg_table_row.cohead_saletype_id, tg_table_row.cohead_id]);
-      }
+      ELSE IF (tg_table_name = 'wo') THEN
+         _wftypecode := 'WO';
+         _workflow_class  := 'XM.WorkOrderWorkflow'
+         _item_uuid := tg_table_row.obj_uuid;
+         SELECT warehous_sitetype_id AS parent_id INTO _parent_id FROM warehous WHERE warehous_id = tg_table_row.wo_itemsite_id;
+         IF (NOT FOUND) THEN
+           RAISE WARNING, "Missing parentId needed to generate workflow!";
+         END IF;
+         _order_id = tg_table_row.wo_id;
+      END IF;
+      
+      -- Get _source_model
+      SELECT wftype_src_tblname AS srctblname FROM xt.wftype WHERE wftype_code = _wftypecode INTO _source_model;
+      IF (NOT FOUND) THEN
+        RAISE WARNING, "Missing sourceModel needed to generate workflow!";
+      END IF;
+     
+      -- Generate workflow 
+      PERFORM xt.workflow_inheritsource('xt.' || _source_model, _workflow_class, _item_uuid, _parent_id, _order_id);
 
-      if (tg_table_name === 'prj') {
-        sourceModel = plv8.execute(sourceModSql, ['PRJ'])[0].srctblname;
+      RETURN tg_table_row;
+  END IF;
 
-        if (!sourceModel) {
-          plv8.elog(WARNING, "Missing sourceModel and/or parentId needed to generate workflow!");
-        }
-        plv8.execute("SELECT xt.workflow_inheritsource($1, $2, $3, $4, $5);",
-          ["xt." + sourceModel, 'XM.ProjectWorkflow', tg_table_row.obj_uuid, tg_table_row.prj_prjtype_id, tg_table_row.prj_id]);
-      }
-
-      if (tg_table_name === 'poheadext') {
-        sourceModel = plv8.execute(sourceModSql, ['PO'])[0].srctblname;
-        parentIdSql = "select poheadext_potype_id as parent_id, pohead_id, pohead.obj_uuid as pohead_uuid " +
-          "from xt.poheadext join pohead on poheadext_id = pohead_id where poheadext_id = $1";
-        parent = plv8.execute(parentIdSql, [tg_table_row.poheadext_id])[0];
-        if (!sourceModel || !parent) {
-          plv8.elog(WARNING, "Missing sourceModel and/or parentId needed to generate workflow!");
-        }
-        plv8.execute("SELECT xt.workflow_inheritsource($1, $2, $3, $4, $5);",
-          ["xt." + sourceModel, 'XM.PurchaseOrderWorkflow', parent.pohead_uuid, parent.parent_id, parent.pohead_id]);
-      }
-
-      if (tg_table_name === 'tohead') {
-        sourceModel = plv8.execute(sourceModSql, ['TO'])[0].srctblname;
-        parentIdSql = "select warehous_sitetype_id as parent_id from warehous where warehous_id = $1";
-        parentId = plv8.execute(parentIdSql, [tg_table_row.tohead_src_warehous_id])[0].parent_id;
-
-        if (!sourceModel || !parentId) {
-          plv8.elog(WARNING, "Missing sourceModel and/or parentId needed to generate workflow!");
-        }
-        plv8.execute("SELECT xt.workflow_inheritsource($1, $2, $3, $4, $5);",
-          ["xt." + sourceModel, 'XM.TransferOrderWorkflow', tg_table_row.obj_uuid, parentId, tg_table_row.tohead_id]);
-      }
-
-      if (tg_table_name === 'wo') {
-        sourceModel = plv8.execute(sourceModSql, ['WO'])[0].srctblname;
-        parentIdSql = "select itemsite_plancode_id as parent_id from itemsite where itemsite_id = $1";
-        parentId = plv8.execute(parentIdSql, [tg_table_row.wo_itemsite_id])[0].parent_id;
-
-        if (!sourceModel || !parentId) {
-          plv8.elog(WARNING, "Missing sourceModel and/or parentId needed to generate workflow!");
-        }
-        plv8.execute("SELECT xt.workflow_inheritsource($1, $2, $3, $4, $5);", ["xt." + sourceModel, 'XM.WorkOrderWorkflow', tg_table_row.obj_uuid, parentId, tg_table_row.wo_id]);
-      }
-
-      return tg_table_row;
-  }
-
-}());
+END;
 
 $BODY$
-  LANGUAGE plv8 VOLATILE
+  LANGUAGE plpgsql VOLATILE
   COST 100;
 ALTER FUNCTION xt.createwf(text, record)
   OWNER TO admin;
--- database/source/xt/trigger_functions/createwf_after_insert.sql
+++ database/source/xt/trigger_functions/createwf_after_insert.sql
@@ -1,46 +1,44 @@
-CREATE OR REPLACE FUNCTION createwf_after_insert() RETURNS TRIGGER AS $createwf_after_insert$
+CREATE OR REPLACE FUNCTION xt.createwf_after_insert() RETURNS TRIGGER AS $$
     BEGIN
 /* Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
    See www.xm.ple.com/CPAL for the full text of the software license. */
 
-return (function () {
-    if (TG_OP === 'INSERT') {
-      createwf(TG_TABLE_NAME, NEW);
-      return NEW;
-    }
-}());
+    IF (TG_OP === 'INSERT') THEN
+      PERFORM xt.createwf(TG_TABLE_NAME, NEW);
+      RETURN NEW;
+    END IF;
+END;
 
-$BODY$
-  LANGUAGE plv8 VOLATILE
+$$ LANGUAGE plpgsql VOLATILE
   COST 100;
 ALTER FUNCTION xt.createwf_after_insert()
   OWNER TO admin;
 
-CREATE OR REPLACE FUNCTION updatewf_after_update() RETURNS TRIGGER AS $updatewf_after_update$
-    BEGIN
+CREATE OR REPLACE FUNCTION xt.updatewf_after_update() RETURNS TRIGGER AS $$
+   BEGIN
 /* Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
    See www.xm.ple.com/CPAL for the full text of the software license. */
 
-return (function () {
-    if (TG_OP === 'UPDATE') {
+   IF (TG_OP === 'UPDATE') THEN
       DELETE FROM xt.wf WHERE parent_obj_uuid = NEW.obj_uuid;
-      createwf(TG_TABLE_NAME, NEW);
-      return NEW;
-    }
-}());
+      PERFORM xt.createwf(TG_TABLE_NAME, NEW);
+      RETURN NEW;
+   END IF;
+END;
 
-$BODY$
-  LANGUAGE plv8 VOLATILE
+$$ LANGUAGE plpgsql VOLATILE
   COST 100;
 ALTER FUNCTION xt.updatewf_after_update()
   OWNER TO admin;
-  
+
+DROP TRIGGER sowf_after_insert ON cohead;
+ 
 CREATE TRIGGER sowf_after_insert
   AFTER INSERT
   ON cohead
   FOR EACH ROW
   EXECUTE PROCEDURE xt.createwf_after_insert();
-  
+
 CREATE TRIGGER sowf_after_update
   AFTER UPDATE
   ON cohead
