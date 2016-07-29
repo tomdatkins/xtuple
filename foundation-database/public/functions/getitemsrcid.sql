a282cc455e0f51164c3f806ef0da43b0c7dd8a12
garyhgohoos <gary@xtuple.com>
2016-07-29 10:50:57 -0400

    Issue #28249:add vendor_item_number to itemsrc selection

--- foundation-database/public/functions/getitemsrcid.sql
+++ foundation-database/public/functions/getitemsrcid.sql
@@ -1,9 +1,8 @@
-CREATE OR REPLACE FUNCTION getItemSrcId(text,text) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION getItemSrcId(pItemNumber TEXT,
+                                        pVendNumber TEXT) RETURNS INTEGER AS $$
 -- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  pItemNumber ALIAS FOR $1;
-  pVendNumber ALIAS FOR $2;
   _returnVal INTEGER;
 BEGIN
   IF ((pItemNumber IS NULL) OR (pVendNumber IS NULL)) THEN
@@ -16,9 +15,35 @@ BEGIN
   AND (itemsrc_vend_id=getVendId(pVendNumber)));
 
   IF (_returnVal IS NULL) THEN
-	RAISE EXCEPTION ''Item Source Item % Vendor % not found.'', pItemNumber,pVendNumber;
+	RAISE EXCEPTION 'Item Source Item % Vendor % not found.', pItemNumber,pVendNumber;
   END IF;
 
   RETURN _returnVal;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION getItemSrcId(pItemNumber TEXT,
+                                        pVendNumber TEXT,
+                                        pVendItemNumber TEXT) RETURNS INTEGER AS $$
+-- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+-- See www.xtuple.com/CPAL for the full text of the software license.
+DECLARE
+  _returnVal INTEGER;
+BEGIN
+  IF ((pItemNumber IS NULL) OR (pVendNumber IS NULL) OR (pVendItemNumber IS NULL)) THEN
+    RETURN NULL;
+  END IF;
+
+  SELECT itemsrc_id INTO _returnVal
+  FROM itemsrc
+  WHERE ((itemsrc_item_id=getItemId(pItemNumber))
+  AND (itemsrc_vend_id=getVendId(pVendNumber))
+  AND (itemsrc_vend_item_number=pVendItemNumber));
+
+  IF (_returnVal IS NULL) THEN
+	RAISE EXCEPTION 'Item Source Item % Vendor % Vend Item % not found.', pItemNumber,pVendNumber,pVendItemNumber;
+  END IF;
+
+  RETURN _returnVal;
+END;
+$$ LANGUAGE plpgsql;
