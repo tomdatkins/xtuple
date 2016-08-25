288d6cf53cff84e061a2f6dac9a3c70e00085a08
Gil Moskowitz <gilmoskowitz@acm.org>
2016-08-25 13:13:04 -0400

    issue #26919 - connect New button on Work Order Operations to selection validity

--- source/manufacturing/foundation-database/xtmfg/tables/pkgscript/workOrderOperations.js
+++ source/manufacturing/foundation-database/xtmfg/tables/pkgscript/workOrderOperations.js
@@ -1,6 +1,6 @@
 /*
   This file is part of the xtmfg Package for xTuple ERP,
-  and is Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.  It
+  and is Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.  It
   is licensed to you under the xTuple End-User License Agreement ("the
   EULA"), the full text of which is available at www.xtuple.com/EULA.
   While the EULA gives you access to source code and encourages your
@@ -232,8 +232,9 @@ if(privileges.check("MaintainWoOperations"))
 else
 {
   _wooper.itemSelected.connect(_view, "animateClick");
-  _new.enabled=false;
 }
+_new.enabled=false;
 
 mainwindow.workOrderOperationsUpdated.connect(sCatchOperationsUpdated);
+_wo["valid(bool)"].connect(_new, "setEnabled(bool)");
 _wo.setFocus();
