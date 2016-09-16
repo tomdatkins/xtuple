dce427e6db6d8938c1e83e9440af585899c11945
Gil Moskowitz <gilmoskowitz@acm.org>
2016-09-16 15:32:56 -0400

    bump version in anticipation of future release

--- source/manufacturing/foundation-database/update_version.sql
+++ source/manufacturing/foundation-database/update_version.sql
@@ -1,2 +1,9 @@
-UPDATE pkghead SET pkghead_version = '4.10.0RC' WHERE pkghead_name = 'xtmfg';
+select createpkgschema('xtmfg', 'This file is part of the xtmfg Package for xTuple ERP, and is Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.  It is licensed to you under the xTuple End-User License Agreement ("the EULA"), the full text of which is available at www.xtuple.com/EULA.  While the EULA gives you access to source code and encourages your involvement in the development process, this Package is not free software.  By using this software, you agree to be bound by the terms of the EULA.');
+
+UPDATE pkghead SET pkghead_descrip   = 'xTuple Manufacturing Edition package.',
+                   pkghead_developer = 'xTuple',
+                   pkghead_indev     = false,
+                   pkghead_version   = '4.11.0Beta'
+ WHERE pkghead_name = 'xtmfg';
+
 DELETE FROM xtmfg.pkgscript where script_name = 'databaseInformation';
