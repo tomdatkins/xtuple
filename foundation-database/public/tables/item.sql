c98b67043953151f3fc702d392674f54e788f864
Dave Anderson (xTuple) <danderson@xtuple.com>
2016-10-14 13:18:20 +1300

    Correct item UPC code prior to applying unique contraint.  Convert '' to NULL

--- foundation-database/public/tables/item.sql
+++ foundation-database/public/tables/item.sql
@@ -1,4 +1,8 @@
 select xt.add_column('item', 'item_created',      'TIMESTAMP WITH TIME ZONE', NULL, 'public');
 select xt.add_column('item', 'item_lastupdated',  'TIMESTAMP WITH TIME ZONE', NULL, 'public');
 
+
+-- Clean up UPC code prior to making UNIQUE
+UPDATE item SET item_upccode = NULL WHERE item_upccode = '';
+
 select xt.add_constraint('item', 'item_item_upccode_key', 'UNIQUE(item_upccode)', 'public');
