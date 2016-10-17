07ec970fd79d8b75cde0d9ce036652c040f3b931
Chris Cotnoir <ccotnoir@xtuple.com>
2016-10-17 14:31:10 -0400

    Allow xt.add_column to modify type and column constraints of existing columns

--- lib/orm/source/xt/functions/add_column.sql
+++ lib/orm/source/xt/functions/add_column.sql
@@ -20,12 +20,34 @@ begin
   get diagnostics count = row_count;
 
   if (count > 0) then
-    return false;
+    query = 'alter table ' || schema_name || '.' || table_name || ' alter column ' || column_name || ' set data type ' || type_name;
+
+    execute query;
+    if (substring(lower(coalesce(constraint_text, '')) from 'not null') is not null) then
+      query = 'alter table ' || schema_name || '.' || table_name || ' alter column ' || column_name || ' set not null';
+
+      execute query;
+      constraint_text = regexp_replace(constraint_text, 'not null', '', 'i');
+    else
+      query = 'alter table ' || schema_name || '.' || table_name || ' alter column ' || column_name || ' drop not null';
+
+      execute query;
+      constraint_text = regexp_replace(constraint_text, 'null', '', 'i');
+    end if;
+    if (substring(lower(coalesce(constraint_text, '')) from 'default') is not null) then
+      query = 'alter table ' || schema_name || '.' || table_name || ' alter column ' || column_name || ' set default ' || regexp_replace(constraint_text, 'default', '', 'i');
+
+      execute query;
+    else
+      query = 'alter table ' || schema_name || '.' || table_name || ' alter column ' || column_name || ' drop default';
+
+      execute query;
+    end if;
+  else
+    query = 'alter table ' || schema_name || '.' || table_name || ' add column ' || column_name || ' ' || type_name || ' ' || coalesce(constraint_text, '');
+
+    execute query;
   end if;
-
-  query = 'alter table ' || schema_name || '.' || table_name || ' add column ' || column_name || ' ' || type_name || ' ' || coalesce(constraint_text, '');
-
-  execute query;
 
   if (column_comment is not null) then
     comment_query = 'comment on column ' || schema_name || '.' || table_name || '.' || column_name || ' is ' || quote_literal(column_comment);
