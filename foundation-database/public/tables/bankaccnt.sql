0f484f63caa6480fcfcdd8600c36649f50d90205
Chris Cotnoir <ccotnoir@xtuple.com>
2016-10-18 13:54:21 -0400

    Make prefix specific to bank account

--- foundation-database/public/tables/bankaccnt.sql
+++ foundation-database/public/tables/bankaccnt.sql
@@ -1,1 +1,2 @@
+select xt.add_column('bankaccnt','bankaccnt_ach_prefix', 'TEXT', '', 'public');
 select xt.add_column('bankaccnt','bankaccnt_prnt_check', 'BOOLEAN', 'NOT NULL DEFAULT TRUE', 'public');
