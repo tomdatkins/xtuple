080639cba4c46567b2be0b57c8bce0656cff62c5
BC Wilson <bc@xtuple.com>
2016-10-25 11:06:40 -0400

    fixes to remove workflow from xtuple (non-commercial)

--- enyo-client/extensions/source/purchasing/database/source/xt/tables/poheadext.sql
+++ enyo-client/extensions/source/purchasing/database/source/xt/tables/poheadext.sql
@@ -5,8 +5,3 @@ select xt.add_column('poheadext','poheadext_potype_id', 'integer');
 select xt.add_constraint('poheadext', 'poheadext_potype_id_fkey', 'foreign key (poheadext_potype_id) references xt.potype (potype_id)');
 
 comment on table xt.poheadext is 'Purchase Order extension table';
-
--- auto workflow generation trigger
-drop trigger if exists powf_after_insert on xt.poheadext;
-create trigger powf_after_insert after insert on xt.poheadext for each row
-execute procedure xt.createwf_after_insert();
