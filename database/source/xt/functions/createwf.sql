d4a0961f4b79fb9ae31cb90da3e7550f86531a4f
Gil Moskowitz <gilmoskowitz@acm.org>
2016-11-18 09:59:57 -0500

    more workflow backports to 4.10
    including cherry pick 497c4d3

--- database/source/xt/functions/createwf.sql
+++ database/source/xt/functions/createwf.sql
@@ -8,52 +8,62 @@ $BODY$
 /* Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.
    See www.xm.ple.com/CPAL for the full text of the software license. */
 DECLARE
-tg_table_name ALIAS FOR $1; 
+tg_table_name ALIAS FOR $1;
 tg_table_row  ALIAS FOR $2;
 _wftypecode   TEXT      := '';
 _source_model TEXT      := '';
 _workflow_class TEXT    := '';
+_default_hold_type CHARACTER(1);
 _item_uuid    UUID;
 _parent_id    INTEGER   := 0;
 _order_id     INTEGER   := 0;
 _poparent     RECORD;
+_saletypeextExists BOOLEAN := false;
 
 BEGIN
 
    IF (fetchmetricbool('TriggerWorkflow')) THEN
-            
-      -- Get data by type      
+
+      -- Get data by type
       IF (tg_table_name = 'cohead') THEN
-         _wftypecode := 'SO';
-         _workflow_class  := 'XM.SalesOrderWorkflow';
-         _item_uuid := tg_table_row.obj_uuid;
-         _parent_id := tg_table_row.cohead_saletype_id;
-         _order_id := tg_table_row.cohead_id;
-         
+        _wftypecode := 'SO';
+        _workflow_class  := 'XM.SalesOrderWorkflow';
+        _item_uuid := tg_table_row.obj_uuid;
+        _parent_id := tg_table_row.cohead_saletype_id;
+        _order_id := tg_table_row.cohead_id;
+
+        -- Copy SaleType char to Sales Order.
+        PERFORM xt.copychars('ST', _parent_id, 'S', _order_id);
+
+        -- Switch `cohead_holdtype` to the `saletypeext_default_hold_type`.
+        SELECT EXISTS (
+          SELECT 1
+          FROM   pg_catalog.pg_class c
+          JOIN   pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+          WHERE  n.nspname = 'xt'
+          AND    c.relname = 'saletypeext'
+          AND    c.relkind = 'r' -- table
+        ) INTO _saletypeextExists;
+
+        IF (_saletypeextExists) THEN
+          SELECT
+            saletypeext_default_hold_type INTO _default_hold_type
+          FROM xt.saletypeext
+          WHERE saletypeext_id = _parent_id;
+
+          IF ((_default_hold_type IS NOT NULL) AND (_order_id > 0)) THEN
+            UPDATE cohead SET
+              cohead_holdtype = _default_hold_type
+            WHERE cohead_id = _order_id;
+          END IF;
+        END IF;
+
       ELSIF (tg_table_name = 'prj') THEN
          _wftypecode := 'PRJ';
          _workflow_class  := 'XM.ProjectWorkflow';
          _item_uuid := tg_table_row.obj_uuid;
          _parent_id := tg_table_row.prj_prjtype_id;
          _order_id := tg_table_row.prj_id;
-         
-      ELSIF (tg_table_name = 'pohead') THEN
-         _wftypecode := 'PO';
-         _workflow_class  := 'XM.PurchaseOrderWorkflow';
-         SELECT poheadext_potype_id AS parent_id, pohead_id, 
-           pohead.obj_uuid AS pohead_uuid, 
-           pohead_status INTO _poparent
-           FROM pohead JOIN xt.poheadext ON poheadext_id = pohead_id 
-           WHERE pohead_id = tg_table_row.pohead_id;
-         IF (NOT FOUND) THEN
-           RAISE WARNING 'Missing parentId needed to generate workflow!';
-         END IF;
-         IF (_poparent.pohead_status <> 'O') THEN 
-           RETURN tg_table_row;
-         END IF;
-         _item_uuid    := _poparent.pohead_uuid;
-         _parent_id    := _poparent.parent_id;
-         _order_id     := _poparent.pohead_id;
 
       ELSIF (tg_table_name = 'poheadext') THEN
          _wftypecode := 'PO';
@@ -100,14 +110,14 @@ BEGIN
       ELSE
         RAISE WARNING 'No table name supplied to createwf function';
       END IF;
-      
+
       -- Get _source_model
       SELECT wftype_src_tblname AS srctblname FROM xt.wftype WHERE wftype_code = _wftypecode INTO _source_model;
       IF (NOT FOUND) THEN
         RAISE WARNING 'Missing sourceModel needed to generate workflow!';
       END IF;
-     
-      -- Generate workflow 
+
+      -- Generate workflow
       PERFORM xt.workflow_inheritsource('xt.' || _source_model, _workflow_class, _item_uuid, _parent_id, _order_id);
 
   END IF;
