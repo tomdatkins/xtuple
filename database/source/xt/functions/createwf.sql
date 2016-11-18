b1a563cbdc0f5fe12e47b28736b9148f446e2ba2
BC Wilson <bc@xtuple.com>
2016-11-18 22:31:32 +0000

    Add code from 4_11_x to handle pohead table

--- database/source/xt/functions/createwf.sql
+++ database/source/xt/functions/createwf.sql
@@ -65,6 +65,24 @@ BEGIN
          _parent_id := tg_table_row.prj_prjtype_id;
          _order_id := tg_table_row.prj_id;
 
+      ELSIF (tg_table_name = 'pohead') THEN
+         _wftypecode := 'PO';
+         _workflow_class  := 'XM.PurchaseOrderWorkflow';
+         SELECT poheadext_potype_id AS parent_id, pohead_id,
+           pohead.obj_uuid AS pohead_uuid,
+           pohead_status INTO _poparent
+           FROM pohead JOIN xt.poheadext ON poheadext_id = pohead_id
+           WHERE pohead_id = tg_table_row.pohead_id;
+         IF (NOT FOUND) THEN
+           RAISE WARNING 'Missing parentId needed to generate workflow!';
+         END IF;
+         IF (_poparent.pohead_status <> 'O') THEN
+           RETURN tg_table_row;
+         END IF;
+         _item_uuid    := _poparent.pohead_uuid;
+         _parent_id    := _poparent.parent_id;
+         _order_id     := _poparent.pohead_id;
+
       ELSIF (tg_table_name = 'poheadext') THEN
          _wftypecode := 'PO';
          _workflow_class  := 'XM.PurchaseOrderWorkflow';
