6c6dff2b8cb56e237109ad834ee7f4ad6115b275
Dave Anderson (xTuple) <danderson@xtuple.com>
2017-01-04 09:37:47 +1300

    Fix manifest errors and also resolve idempotency of changes

--- foundation-database/public/tables/cohead.sql
+++ foundation-database/public/tables/cohead.sql
@@ -1,11 +1,23 @@
 DO $$
+DECLARE
+  _col INTEGER;
 BEGIN
 
--- Check if column is being used and if not remove
+  _col := (SELECT 1
+           FROM information_schema.columns
+           WHERE table_name='cohead'
+           AND column_name='cohead_type');
+
+  IF NOT FOUND THEN
+     RETURN;
+  END IF;
+
+-- Check if column is being used and if not used remove column
   IF (NOT EXISTS(SELECT 1 FROM cohead
              WHERE cohead_type IS NOT NULL)) THEN
 
     ALTER TABLE public.cohead DROP COLUMN IF EXISTS cohead_type;
   END IF;
+
 END;
-$$
+$$ language plpgsql;
