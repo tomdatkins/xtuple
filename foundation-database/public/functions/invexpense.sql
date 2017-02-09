4e49febf3399b800d436714d7bae8daadb926e6b
jcarlin <jcarlin@xtuple.com>
2017-02-08 19:40:28 -0600

    Expense Transaction working

--- foundation-database/public/functions/invexpense.sql
+++ foundation-database/public/functions/invexpense.sql
@@ -1,3 +1,5 @@
+DROP FUNCTION IF EXISTS invExpense(INTEGER, NUMERIC, INTEGER, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, INTEGER);
+
 CREATE OR REPLACE FUNCTION invExpense(pItemsiteid     INTEGER,
                                       pQty            NUMERIC,
                                       pExpcatid       INTEGER,
@@ -5,9 +7,10 @@ CREATE OR REPLACE FUNCTION invExpense(pItemsiteid     INTEGER,
                                       pComments       TEXT,
                                       pGlDistTS       TIMESTAMP WITH TIME ZONE
                                                       DEFAULT CURRENT_TIMESTAMP,
-                                      pPrjid          INTEGER DEFAULT NULL)
+                                      pPrjid          INTEGER DEFAULT NULL,
+                                      pItemlocSeries  INTEGER DEFAULT NULL)
   RETURNS INTEGER AS $$
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+-- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   _invhistid INTEGER;
@@ -23,14 +26,16 @@ BEGIN
     RETURN 0;
   END IF;
 
-  SELECT NEXTVAL('itemloc_series_seq') INTO _itemlocSeries;
+  -- pItemlocSeries is passed from expenseTrans.cpp post #22868. 
+  -- pItemlocSeries prevents postInvTrans (pPreDistributed) from creating itemlocdist records again.
+  _itemlocSeries := COALESCE(pItemlocSeries, NEXTVAL('itemloc_series_seq'));
   SELECT postInvTrans( itemsite_id, 'EX', pQty,
                        'I/M', 'EX', pDocumentNumber, '',
                        CASE WHEN (pQty < 0) THEN ('Reverse Material Expense for item ' || item_number || E'\n' ||  pComments)
                             ELSE  ('Material Expense for item ' || item_number || E'\n' ||  pComments)
                        END,
                        getPrjAccntId(pPrjid, expcat_exp_accnt_id), costcat_asset_accnt_id,
-                       _itemlocSeries, pGlDistTS) INTO _invhistid
+                       _itemlocSeries, pGlDistTS, NULL, NULL, NULL, COALESCE(pItemlocSeries, 0) != 0) INTO _invhistid
   FROM itemsite, item, costcat, expcat
   WHERE ( (itemsite_item_id=item_id)
    AND (itemsite_costcat_id=costcat_id)
@@ -43,4 +48,4 @@ BEGIN
   RETURN _itemlocSeries;
 
 END;
-$$ LANGUAGE 'plpgsql';
+$$ LANGUAGE plpgsql;
