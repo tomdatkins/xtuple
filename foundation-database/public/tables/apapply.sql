2ab5adc82991df322bb283c375a256cf0ddcd2ff
Dave Anderson (xTuple) <danderson@xtuple.com>
2017-03-07 12:21:18 +1300

    Issue #29303: Add feature to reverse AR/AP Applications

--- foundation-database/public/tables/apapply.sql
+++ foundation-database/public/tables/apapply.sql
@@ -1,3 +1,4 @@
 select xt.add_column('apapply','apapply_target_curr_id', 'INTEGER', NULL, 'public');
+select xt.add_column('apapply','apapply_reversed', 'BOOLEAN', 'NOT NULL DEFAULT false', 'public');
 
 select xt.add_constraint('apapply','apapply_target_curr_id_fkey', 'foreign key (apapply_target_curr_id) references curr_symbol(curr_id)', 'public');
