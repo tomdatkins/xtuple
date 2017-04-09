c3f961ecd550071dfea149c65b12711c92fd4ea6
Dave Anderson (xTuple) <danderson@xtuple.com>
2017-04-10 10:03:13 +1200

    Handle empty Document Reference when reversing AR applications

--- foundation-database/public/functions/reversearapplication.sql
+++ foundation-database/public/functions/reversearapplication.sql
@@ -79,20 +79,20 @@ BEGIN
 
   IF (_r.arapply_source_doctype = 'K') THEN
     INSERT INTO aropen(
-            aropen_docdate, aropen_duedate,  aropen_cust_id, 
-            aropen_doctype, aropen_docnumber, 
+            aropen_docdate, aropen_duedate,  aropen_cust_id,
+            aropen_doctype, aropen_docnumber,
             aropen_amount, aropen_curr_rate, aropen_curr_id,
-            aropen_notes, 
-             aropen_ponumber, 
-             aropen_paid, aropen_open, aropen_username, 
-            aropen_discount, aropen_accnt_id,  
+            aropen_notes,
+             aropen_ponumber,
+             aropen_paid, aropen_open, aropen_username,
+            aropen_discount, aropen_accnt_id,
             aropen_distdate
             )
-    VALUES (_r.arapply_postdate, current_date, _r.arapply_cust_id,  
-            'C', _r.arapply_refnumber, 
+    VALUES (_r.arapply_postdate, current_date, _r.arapply_cust_id,
+            'C', COALESCE(NULLIF(_r.arapply_refnumber, ''), _r.arapply_source_docnumber),
             _r.aamt, currrate(_r.arapply_curr_id, _r.arapply_postdate), _r.arapply_curr_id,
-             'Payment ' || _r.arapply_source_docnumber || _r.arapply_refnumber || ' to be re-applied; original date: ' || _r.arapply_postdate, 
-            '', 0, true, geteffectivextuser(), false, -1, 
+             'Payment ' || _r.arapply_source_docnumber || _r.arapply_refnumber || ' to be re-applied; original date: ' || _r.arapply_postdate,
+            '', 0, true, geteffectivextuser(), false, -1,
             current_date);
   END IF;
 
