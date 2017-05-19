c36d11190f7e2b64404acaa093a7ad521c3b6ddf
Dave Anderson (xTuple) <danderson@xtuple.com>
2017-05-19 16:48:09 +1200

    Issue #29882: Default user menus from assigned role

---
+++ foundation-database/public/tables/grp.sql
@@ -0,0 +1,17 @@
+select xt.add_column('grp', 'grp_showimmenu', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showpdmenu', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showmsmenu', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showwomenu', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showcrmmenu', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showpomenu', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showsomenu', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showglmenu', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showimtoolbar', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showpdtoolbar', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showmstoolbar', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showwotoolbar', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showcrmtoolbar', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showpotoolbar', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showsotoolbar', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+select xt.add_column('grp', 'grp_showgltoolbar', 'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public');
+
