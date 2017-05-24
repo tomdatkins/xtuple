19975ba5bb2ac06f3ee13826b3168aa306bc5620
Chris Cotnoir <ccotnoir@xtuple.com>
2017-05-24 14:25:17 -0400

    Added unique constraint to potype table

--- foundation-database/public/tables/potype.sql
+++ foundation-database/public/tables/potype.sql
@@ -8,6 +8,8 @@ select xt.add_column('potype','potype_active', 'boolean', 'NOT NULL DEFAULT TRUE
 select xt.add_column('potype','potype_default','boolean', 'NOT NULL DEFAULT FALSE', 'public');
 select xt.add_column('potype','potype_emlprofile_id', 'integer', null, 'public');
 
+select xt.add_constraint('potype', 'potype_potype_code_key', 'UNIQUE (potype_code)', 'public');
+
 comment on table public.potype is 'Purchase Order Type table';
 
 -- Migrate data if exists
