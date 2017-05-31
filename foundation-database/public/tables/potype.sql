a8439acfa0fb28c53c25fa504c7ed6b7dc2c604f
Gil Moskowitz <gilmoskowitz@acm.org>
2017-05-31 17:35:57 -0400

    issue #30214 - move the table if possible

--- foundation-database/public/tables/potype.sql
+++ foundation-database/public/tables/potype.sql
@@ -1,4 +1,15 @@
 -- Migrate POTYPE table from original xt extension into public
+DO $$
+BEGIN
+  IF EXISTS(SELECT 1
+              FROM information_schema.tables 
+             WHERE table_schema = 'xt'
+               AND table_name = 'potype') THEN
+    ALTER TABLE xt.potype SET SCHEMA public;
+  END IF;
+END
+$$ language plpgsql;
+
 select xt.create_table('potype', 'public');
 
 select xt.add_column('potype','potype_id', 'serial', 'primary key', 'public');
@@ -9,24 +20,3 @@ select xt.add_column('potype','potype_default','boolean', 'NOT NULL DEFAULT FALS
 select xt.add_column('potype','potype_emlprofile_id', 'integer', null, 'public');
 
 comment on table public.potype is 'Purchase Order Type table';
-
--- Migrate data if exists
-DO $$
-BEGIN
-  IF EXISTS(SELECT 1
-              FROM information_schema.tables 
-             WHERE table_schema = 'xt'
-               AND table_name = 'potype') THEN
-
-    INSERT INTO public.potype (potype_id, potype_code, potype_descr, potype_active, potype_emlprofile_id)
-      SELECT potype_id, potype_code, potype_descr, potype_active, potype_emlprofile_id
-      FROM xt.potype;
-      
-    ALTER TABLE IF EXISTS xt.vendinfoext DROP CONSTRAINT IF EXISTS vendinfoext_potype_id_fkey;
-    ALTER TABLE IF EXISTS xt.poheadext   DROP CONSTRAINT IF EXISTS poheadext_potype_id_fkey;
-
-    DROP TABLE xt.potype;
-    
-  END IF;
-END
-$$ language plpgsql;
