d58f26ebeb3a19480ebf830cb98491aee8474c5c
tomdatkins <tom@xtuple.com>
2017-06-13 14:33:12 -0400

    26880_delete_comm_codes
    Added Right click delete ( along with edit and view) and fixed the
    delete button.

--- source/distribution/foundation-database/xwd/tables/pkgscript/catComms.js
+++ source/distribution/foundation-database/xwd/tables/pkgscript/catComms.js
@@ -1,6 +1,6 @@
 /*
   This file is part of the xwd Package for xTuple ERP,
-  and is Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.  It
+  and is Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple.  It
   is licensed to you under the xTuple End-User License Agreement ("the
   EULA"), the full text of which is available at www.xtuple.com/EULA.
   While the EULA gives you access to source code and encourages your
@@ -8,7 +8,7 @@
   software.  By using this software, you agree to be bound by the
   terms of the EULA.
 */
-
+debugger;
 include("storedProcErrorLookup");
 include("xwdErrors");
 
@@ -28,8 +28,7 @@ try {
 
   _new.clicked.connect(sNew);
   _edit.clicked.connect(sEdit);
-//  _delete.clicked.connect(sDelete);
-  _delete.hide();
+  _delete.clicked.connect(sDelete);
   _view.clicked.connect(sView);
 
   _close.clicked.connect(mywindow, "close");
@@ -37,7 +36,7 @@ try {
   if(privileges.check("MaintainCatalogConfig"))
   {
     _catcomm.valid.connect(_edit, "setEnabled");
-//    _catcomm.valid.connect(_delete, "setEnabled");
+    _catcomm.valid.connect(_delete, "setEnabled");
     _catcomm.itemSelected.connect(_edit, "animateClick");
   }
   else
@@ -51,6 +50,36 @@ try {
   QMessageBox.critical(mywindow, "catComms",
                        "catComms.js exception: " + e);
 }
+_catcomm["populateMenu(QMenu *, XTreeWidgetItem *, int)"].connect(sPopulateMenu)
+
+function sPopulateMenu(pMenu, pItem, pCol)
+    {
+      try
+          {
+            if(pMenu == null)
+            pMenu = _list.findChild("_menu");  
+            if(pMenu != null)
+            {
+              tmpact = pMenu.addAction(qsTr("Edit..."));
+              tmpact.enabled = (privileges.check("MaintainCatalogConfig"));
+              tmpact.triggered.connect(sEdit);
+
+              tmpact = pMenu.addAction(qsTr("View..."));
+              tmpact.enabled = (privileges.check("MaintainCatalogConfig"));
+              tmpact.triggered.connect(sView);
+
+              tmpact = pMenu.addAction(qsTr("Delete Comm..."));
+              tmpact.enabled = (privileges.check("MaintainCatalogConfig"));
+              tmpact.triggered.connect(sDelete);
+
+            }
+         }
+           catch(e)
+             {
+               QMessageBox.critical(mywindow, "catalogList",
+               "sPopulateMenu exception: " + e);
+             }
+    }
 
 function openCatComm(params)
 {
