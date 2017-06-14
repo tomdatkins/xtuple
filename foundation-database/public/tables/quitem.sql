5134d0f02257cf0b8fc89b7d5e1f13007ea21e2b
Ben Thompson <ben@xtuple.com>
2017-06-14 15:58:50 -0400

    Add sublinenumber to quitem to support enhanced pricing.

--- foundation-database/public/tables/quitem.sql
+++ foundation-database/public/tables/quitem.sql
@@ -1,5 +1,6 @@
-select xt.add_column('quitem', 'quitem_listprice', 'NUMERIC(16,4)', NULL, 'public');
-select xt.add_column('quitem', 'quitem_created',      'TIMESTAMP WITH TIME ZONE', NULL, 'public');
-select xt.add_column('quitem', 'quitem_lastupdated',  'TIMESTAMP WITH TIME ZONE', NULL, 'public');
+SELECT xt.add_column('quitem', 'quitem_subnumber',   'INTEGER',                 'NOT NULL DEFAULT 0', 'public');
+SELECT xt.add_column('quitem', 'quitem_listprice',   'NUMERIC(16,4)',            NULL,                'public');
+SELECT xt.add_column('quitem', 'quitem_created',     'TIMESTAMP WITH TIME ZONE', NULL,                'public');
+SELECT xt.add_column('quitem', 'quitem_lastupdated', 'TIMESTAMP WITH TIME ZONE', NULL,                'public');
 
-comment on column public.quitem.quitem_listprice is 'List price of Item.';
+COMMENT ON COLUMN public.quitem.quitem_listprice IS 'List price of Item.';
