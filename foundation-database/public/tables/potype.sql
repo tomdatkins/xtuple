bb85cf85a8b9bbd19d00ce585d527242397e728c
Gil Moskowitz <gilmoskowitz@acm.org>
2017-06-15 12:25:49 -0400

    issue #30280 - reorg to merge xt and public tables, handling overlap

--- foundation-database/public/tables/potype.sql
+++ foundation-database/public/tables/potype.sql
@@ -1,66 +1,88 @@
 -- Migrate POTYPE table from original xt extension into public
 DO $$
 DECLARE
-  _hasOldXtTable        BOOLEAN;
-  _hasName              BOOLEAN := false;
-  _hasCode              BOOLEAN := false;
-  _hasDescr             BOOLEAN := false;
-  _hasDescrip           BOOLEAN := false;
-  _column               TEXT;
+  _hasXtTable     BOOLEAN;
+  _hasPublicTable BOOLEAN;
+  _publicCols     TEXT;
 BEGIN
-  _hasOldXtTable := EXISTS(SELECT 1
-                             FROM information_schema.tables
-                            WHERE table_schema = 'xt'
-                              AND table_name   = 'potype');
-  FOR _column IN SELECT column_name
-              FROM information_schema.columns
-             WHERE table_schema = 'public'
-               AND table_name   = 'potype' LOOP
-    CASE _column WHEN 'potype_code'    THEN _hasCode    := true;
-                 WHEN 'potype_descr'   THEN _hasDescr   := true;
-                 WHEN 'potype_descrip' THEN _hasDescrip := true;
-                 WHEN 'potype_name'    THEN _hasName    := true;
-                 ELSE RAISE DEBUG 'Unhandled public.potype.% during upgrade',
-                      _column;
-    END CASE;
-  END LOOP;
+  _hasXtTable := EXISTS(SELECT 1
+                          FROM information_schema.tables
+                         WHERE table_schema = 'xt'
+                           AND table_name   = 'potype');
+  _hasPublicTable := EXISTS(SELECT 1
+                          FROM information_schema.tables
+                         WHERE table_schema = 'public'
+                           AND table_name   = 'potype');
 
-  IF _hasName AND _hasCode THEN
+  IF _hasXtTable AND NOT _hasPublicTable THEN
+    ALTER TABLE xt.potype SET SCHEMA public;
+    _hasXtTable := FALSE;  -- don't try to copy from it later
+  END IF;
+
+  PERFORM xt.create_table('potype', 'public');
+
+  PERFORM xt.add_column('potype','potype_id',             'SERIAL', 'PRIMARY KEY',            'public'),
+          xt.add_column('potype','potype_code',             'TEXT', null,                     'public'),
+          xt.add_column('potype','potype_descr',            'TEXT', null,                     'public'),
+          xt.add_column('potype','potype_active',        'BOOLEAN', 'NOT NULL DEFAULT TRUE',  'public'),
+          xt.add_column('potype','potype_default',       'BOOLEAN', 'NOT NULL DEFAULT FALSE', 'public'),
+          xt.add_column('potype','potype_emlprofile_id', 'INTEGER', null,                     'public');
+
+  SELECT string_agg(column_name, ' ') INTO _publicCols
+    FROM information_schema.columns
+   WHERE table_schema = 'public'
+     AND table_name   = 'potype';
+
+  ALTER TABLE public.potype DISABLE TRIGGER ALL;
+
+  IF _publicCols ~ 'potype_name' THEN
     UPDATE public.potype SET potype_code = potype_name
       WHERE COALESCE(potype_code, '') = '';
     ALTER TABLE public.potype DROP COLUMN potype_name;
-  ELSIF _hasName AND NOT _hasCode THEN
-    ALTER TABLE public.potype RENAME COLUMN potype_name    TO potype_code;
   END IF;
 
-  IF _hasDescrip AND _hasDescr THEN
+  IF _publicCols ~ 'potype_descrip' THEN
     UPDATE public.potype SET potype_descr = potype_descrip
       WHERE COALESCE(potype_descr, '') = '';
     ALTER TABLE public.potype DROP COLUMN potype_descrip;
-  ELSIF _hasDescrip and NOT _hasDescr THEN
-    ALTER TABLE public.potype RENAME COLUMN potype_descrip TO potype_descr;
   END IF;
 
-  IF _hasOldXtTable AND _column IS NULL THEN     -- null _column => no public.potype
-    ALTER TABLE xt.potype SET SCHEMA public;
-  ELSIF _hasOldXtTable THEN
+  CREATE TEMPORARY TABLE upgrade_potype (LIKE public.potype) ON COMMIT DROP;
+  INSERT INTO upgrade_potype SELECT * FROM public.potype;
+  DELETE FROM public.potype;
+
+  IF _hasXtTable THEN
+    INSERT INTO public.potype (
+      potype_id, potype_code, potype_descr,
+      potype_active, potype_emlprofile_id
+     ) SELECT potype_id, potype_code, potype_descr,
+              potype_active, potype_emlprofile_id
+         FROM xt.potype;
+    
+    DROP TABLE xt.potype CASCADE;
+  END IF;
+
+  -- copy unique old public.potypes, changing their ids if necessary
+  IF _publicCols IS NOT NULL THEN
     INSERT INTO public.potype (potype_id, potype_code, potype_descr)
                         SELECT potype_id, potype_code, potype_descr
-                          FROM xt.potype;
-    DROP TABLE xt.potype;
+                          FROM upgrade_potype u
+                         WHERE NOT EXISTS (SELECT 1
+                                             FROM potype p
+                                           WHERE p.potype_id   = u.potype_id
+                                              OR p.potype_code = u.potype_code);
+    INSERT INTO public.potype (potype_code, potype_descr)
+                        SELECT potype_code, potype_descr
+                          FROM upgrade_potype
+                         WHERE potype_code NOT IN (SELECT potype_code
+                                                     FROM public.potype);
+    DROP TABLE IF EXISTS upgrade_potype;
   END IF;
 END
 $$ LANGUAGE plpgsql;
 
-select xt.create_table('potype', 'public');
+SELECT xt.add_constraint('potype', 'potype_potype_code_key', 'UNIQUE (potype_code)', 'public');
 
-select xt.add_column('potype','potype_id',       'serial', 'primary key', 'public');
-select xt.add_column('potype','potype_code',       'text', null, 'public') ;
-select xt.add_column('potype','potype_descr',      'text', null, 'public');
-select xt.add_column('potype','potype_active',  'boolean', 'NOT NULL DEFAULT TRUE', 'public');
-select xt.add_column('potype','potype_default', 'boolean', 'NOT NULL DEFAULT FALSE', 'public');
-select xt.add_column('potype','potype_emlprofile_id', 'integer', null, 'public');
+ALTER TABLE public.potype ENABLE TRIGGER ALL;
 
-select xt.add_constraint('potype', 'potype_potype_code_key', 'UNIQUE (potype_code)', 'public');
-
-comment on table public.potype is 'Purchase Order Type table';
+COMMENT ON table public.potype IS 'Purchase Order Type table';
