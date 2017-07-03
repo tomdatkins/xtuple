c01dcd3dfa4ccc11e585a7a18382b24b0c7a5699
Gil Moskowitz <gilmoskowitz@acm.org>
2017-07-03 12:47:53 -0400

    issue #30186 - further tweak after additional testing

--- foundation-database/public/patches/populate_ccpay_card_type.sql
+++ foundation-database/public/patches/populate_ccpay_card_type.sql
@@ -1,6 +1,10 @@
+ALTER TABLE public.ccpay DISABLE TRIGGER ALL;
+
 -- issue #23459 populate new ccpay_card_type col from ccard table
 UPDATE ccpay
    SET ccpay_card_type = ccard_type
   FROM ccard
  WHERE ccard_id = ccpay_ccard_id
    AND ccpay_card_type IS NULL;
+
+ALTER TABLE public.ccpay ENABLE TRIGGER ALL;
