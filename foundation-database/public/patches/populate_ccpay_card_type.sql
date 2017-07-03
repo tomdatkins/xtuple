8c609ca23b8bcf6ef4c7518eec5e8b4b5c203cdb
Gil Moskowitz <gilmoskowitz@acm.org>
2017-07-03 12:29:31 -0400

    issue #30186 - improve perf of ccpay table patch
    not technically part of the original request but this change improves
    performance of the ccpay table upgrade by about 80%, which should
    turn into a performance improvement of overall upgrade by about 10%

--- foundation-database/public/patches/populate_ccpay_card_type.sql
+++ foundation-database/public/patches/populate_ccpay_card_type.sql
@@ -1,3 +1,6 @@
--- Issue #23459 adds ccpay_card_type. Populate it from historical ccard relations.
-UPDATE ccpay SET ccpay_card_type = (SELECT ccard_type FROM ccard WHERE ccard_id = ccpay_ccard_id)
-WHERE ccpay_ccard_id IS NOT NULL;
+-- issue #23459 populate new ccpay_card_type col from ccard table
+UPDATE ccpay
+   SET ccpay_card_type = ccard_type
+  FROM ccard
+ WHERE ccard_id = ccpay_ccard_id
+   AND ccpay_card_type IS NULL;
