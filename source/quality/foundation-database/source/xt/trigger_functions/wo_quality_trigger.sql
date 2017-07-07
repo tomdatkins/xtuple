06057066322bb434cb6a01dd3c655fd675bdb5e7
Dave Anderson <danderson@xtuple.com>
2017-07-07 14:05:27 +1200

    Issue #30471: Resolve timing issues with WO quality test generation (Last Item trigger)

--- source/quality/foundation-database/source/xt/trigger_functions/wo_quality_trigger.sql
+++ source/quality/foundation-database/source/xt/trigger_functions/wo_quality_trigger.sql
@@ -1,29 +1,60 @@
+DROP TRIGGER IF EXISTS woqualityupdatetrigger ON public.wo;
+DROP FUNCTION IF EXISTS xt.woqualitytrigger();
+
 CREATE OR REPLACE FUNCTION xt.woqualitytrigger()
   RETURNS trigger AS $$
--- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple. 
--- See www.xtuple.com/EULA for the full text of the software license.
-BEGIN
+  /* Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple. 
+    See www.xtuple.com/EULA for the full text of the software license.
+  */ 
+return (function () {
 
---  If a WO is closed/recalled also cancel related quality tests
-   IF (TG_OP = 'DELETE' OR (OLD.wo_status = 'R' AND NEW.wo_status = 'E')) THEN
-     UPDATE xt.qthead SET qthead_status = 'C'
-     WHERE qthead_ordtype = 'WO'
-      AND qthead_ordnumber = formatwonumber(OLD.wo_id);
-   END IF;  
+   var relevantPlan,
+       testsToCreate,
+       qualityTestId,
+       orderNumber,
+       orderNumberSql = "SELECT formatwonumber($1) AS wonumber;",
+       updateSql = "UPDATE xt.qthead SET qthead_status = 'C' " +
+                   "WHERE qthead_ordtype = 'WO' " +
+                   "AND qthead_ordnumber = formatwonumber($1) ",
+       selectSQL = "SELECT qpheadass.* FROM xt.qphead " +
+	           "JOIN xt.qpheadass ON (qphead_id=qpheadass_qphead_id) " +
+	           "JOIN itemsite ON (qpheadass_item_id=itemsite_item_id AND qpheadass_warehous_id=itemsite_warehous_id) " +
+	           "WHERE itemsite_id = $1 " +
+	           "AND qphead_rev_status = 'A' " +
+	           "AND qpheadass_prod;";
+                   
+   /*  If a WO is closed/recalled also cancel related quality tests  */
+   if (TG_OP === 'DELETE' || (OLD.wo_status === 'R' && NEW.wo_status === 'E')) {
+     plv8.EXECUTE(updateSql, [OLD.wo_id]);
+   }
 
-   IF (TG_OP = 'DELETE') THEN
-     RETURN OLD;
-   ELSE  
-     RETURN NEW;
-   END IF;
+   if (TG_OP === 'UPDATE' && (OLD.wo_status !== 'C' && NEW.wo_status === 'C')) {
+     relevantPlan = plv8.execute(selectSQL, [NEW.wo_itemsite_id]);
+     relevantPlan.map(function (plan) {
+       var i, options = [];
 
-END;
-$$ LANGUAGE plpgsql;
+       orderNumber = plv8.execute(orderNumberSql, [NEW.wo_id])[0].wonumber;
+        
+       options.frequency = plan.qpheadass_testfreq;
+       options.orderType = 'WO';
+       options.orderNumber = orderNumber;
+       options.orderItem   = NEW.wo_number;
+
+       testsToCreate = XM.Quality.itemQualityTestsRequired(NEW.wo_itemsite_id, plan.qpheadass_qphead_id, plan.qpheadass_freqtype, options);
+       for (i = 0; i < testsToCreate; i++){
+         qualityTestId = XM.Quality.createQualityTest(NEW.wo_itemsite_id, plan.qpheadass_qphead_id,options);
+       }
+     })
+   }
+
+   return TG_OP === 'DELETE' ? OLD : NEW;
+   
+}());
+$$
+  LANGUAGE plv8;
 
 ALTER FUNCTION xt.woqualitytrigger() OWNER TO admin;
 GRANT EXECUTE ON FUNCTION xt.woqualitytrigger() TO xtrole;
-
-DROP TRIGGER IF EXISTS woqualityupdatetrigger ON public.wo;
 
 CREATE TRIGGER woqualityupdatetrigger
   AFTER UPDATE OR DELETE
