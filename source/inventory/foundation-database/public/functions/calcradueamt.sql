c587b0d34a60f5a8170bf8033f0c7901953b20ea
Gil Moskowitz <gilmoskowitz@acm.org>
2017-08-01 13:56:19 -0400

    issue #30582 - tweak DROP FUNCTION for idempotence

--- source/inventory/foundation-database/public/functions/calcradueamt.sql
+++ source/inventory/foundation-database/public/functions/calcradueamt.sql
@@ -1,4 +1,4 @@
-DROP FUNCTION calcRaDueAmt(INTEGER);
+DROP FUNCTION IF EXISTS calcRaDueAmt(INTEGER);
 CREATE OR REPLACE FUNCTION calcRaDueAmt(pRaid INTEGER) RETURNS NUMERIC AS $$
 -- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/EULA for the full text of the software license.
