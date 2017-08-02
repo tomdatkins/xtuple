1f7508c16a7afaa8bc9d2f71e60c75045843667e
David Beauchamp <david@xtuple.com>
2017-08-02 12:53:14 -0400

    Update namespace oid to use BIGINT as we finally have databases which have enough objects to overflow INTEGER

--- foundation-database/public/functions/createpkgschema.sql
+++ foundation-database/public/functions/createpkgschema.sql
@@ -6,11 +6,11 @@ CREATE OR REPLACE FUNCTION createPkgSchema(pname      TEXT,
                                            pdescrip   TEXT    default '',
                                            pdeveloper TEXT    default '',
                                            pindev     BOOLEAN default FALSE
-                                          ) RETURNS INTEGER AS $$
--- Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.
+                                          ) RETURNS BIGINT AS $$
+-- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  _namespaceoid INTEGER := -1;
+  _namespaceoid BIGINT := -1;
   _tabs         TEXT[] := ARRAY['cmd',  'cmdarg', 'image',  'metasql',
                                 'priv', 'report', 'script', 'uiform'] ;
   _pkgtab       TEXT;
