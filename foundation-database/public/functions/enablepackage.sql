eae2cf20d42ec3af26a0f792170d607d882dfc65
Gil Moskowitz <gilmoskowitz@acm.org>
2017-08-29 14:59:17 -0400

    issue #30782, issue #30851 - resolve idempotence issues
    also make formatting and security updates

--- foundation-database/public/functions/enablepackage.sql
+++ foundation-database/public/functions/enablepackage.sql
@@ -1,56 +1,62 @@
-CREATE OR REPLACE FUNCTION enablePackage(TEXT) RETURNS INTEGER AS $$
+DROP FUNCTION IF EXISTS enablePackage(TEXT);
+DROP FUNCTION IF EXISTS enablePackage(INTEGER);
+
+CREATE OR REPLACE FUNCTION enablePackage(pName TEXT) RETURNS INTEGER AS $$
 -- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  ppkgname  ALIAS FOR $1;
-  _i        INTEGER := 0;
-  _tabs     TEXT[] := ARRAY['cmd',  'cmdarg', 'image',  'metasql',
-                            'priv', 'report', 'script', 'uiform', 'dict'];
-  _trigs   RECORD;
-BEGIN
-  IF (version() < 'PostgreSQL 8.2') THEN
-    RETURN -1;
-  END IF;
+  _i      INTEGER := 0;
+  _tabs   TEXT[] := ARRAY['cmd',  'cmdarg', 'image',  'metasql',
+                          'priv', 'report', 'script', 'uiform', 'dict'];
+  _table  TEXT;
 
+BEGIN
   FOR _i IN ARRAY_LOWER(_tabs,1)..ARRAY_UPPER(_tabs,1) LOOP
-    EXECUTE 'ALTER TABLE IF EXISTS ' || ppkgname || '.pkg' || _tabs[_i] ||
-            ' INHERIT public.' || _tabs[_i] || ';';
+    IF NOT EXISTS(SELECT 1
+                    FROM pg_inherits   i
+                    JOIN pg_class  child ON inhrelid           = child.oid
+                    JOIN pg_class parent ON inhparent          = parent.oid
+                    JOIN pg_namespace n  ON child.relnamespace = n.oid
+                   WHERE child.relname  = 'pkg' || _tabs[_i]
+                     AND parent.relname = _tabs[_i]
+                     AND nspname = pName) THEN
+      EXECUTE format('ALTER TABLE IF EXISTS %I.%I INHERIT public.%I;',
+                     pName, 'pkg' || _tabs[_i], _tabs[_i]);
+    END IF;
   END LOOP;
 
--- Also find and ENable package triggers
-  FOR _trigs IN
-     SELECT tgrelid::regclass AS tbl, tgname
-     FROM pg_proc
-      JOIN pg_namespace ON (pronamespace=pg_namespace.oid)
-      JOIN pg_type ON (prorettype=pg_type.oid)
-      JOIN pg_trigger ON (tgfoid=pg_proc.oid)
-     WHERE nspname=ppkgname
-     AND typname = 'trigger'
+  FOR _table IN
+     SELECT DISTINCT relname
+       FROM pg_trigger   t
+       JOIN pg_class     c ON tgrelid      = c.oid
+       JOIN pg_namespace n ON relnamespace = n.oid
+      WHERE relkind = 'r'
+        AND nspname = pName
   LOOP
-    EXECUTE format('ALTER TABLE %I ENABLE TRIGGER %I', _trigs.tbl, _trigs.tgname);
+    EXECUTE format('ALTER TABLE %I.%I ENABLE TRIGGER ALL;', pName, _table);
   END LOOP;
 
   RETURN 0;
 END;
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION enablePackage(INTEGER) RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION enablePackage(pId INTEGER) RETURNS INTEGER AS $$
 -- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  ppkgheadid    ALIAS FOR $1;
-  _pkgname      TEXT;
+  _result INTEGER;
 
 BEGIN
-  SELECT pkghead_name INTO _pkgname
-  FROM pkghead
-  WHERE (pkghead_id=ppkgheadid);
-  IF (NOT FOUND) THEN
-    RETURN -2;
+  SELECT enablePackage(pkghead_name) INTO _result
+    FROM pkghead
+   WHERE pkghead_id = pId;
+  IF _result IS NULL THEN
+    RAISE EXCEPTION 'enablePackage could not find an extension with id % [xtuple: enablePackage, -2, %]',
+                    pId, pId;
   END IF;
 
-  RETURN enablePackage(_pkgname);
+  RETURN _result;
 END;
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
