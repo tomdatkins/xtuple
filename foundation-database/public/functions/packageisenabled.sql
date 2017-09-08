8f5a58a07c0f7ac3626f015d2b00479df0c2e9f5
Chris Cotnoir <ccotnoir@xtuple.com>
2017-09-08 11:18:02 -0400

    Don't alter pkghead while querying pkghead, rewrite packageisenabled to work properly for schemas with lots of table inheritance not related to packages

--- foundation-database/public/functions/packageisenabled.sql
+++ foundation-database/public/functions/packageisenabled.sql
@@ -1,22 +1,36 @@
-CREATE OR REPLACE FUNCTION packageIsEnabled(INTEGER) RETURNS BOOLEAN AS $$
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+CREATE OR REPLACE FUNCTION packageIsEnabled(pId INTEGER) RETURNS BOOLEAN AS $$
+-- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
-  SELECT COUNT(*) >= 8
-  FROM pg_inherits, pg_class, pg_namespace, pkghead
-  WHERE ((inhrelid=pg_class.oid)
-     AND (relnamespace=pg_namespace.oid)
-     AND  (nspname=lower(pkghead_name))
-     AND  (pkghead_id=$1));
-$$
-LANGUAGE 'sql';
+DECLARE
+  _pkgname TEXT;
 
-CREATE OR REPLACE FUNCTION packageIsEnabled(TEXT) RETURNS BOOLEAN AS $$
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+BEGIN
+
+  SELECT pkghead_name INTO _pkgname
+    FROM pkghead
+   WHERE pkghead_id=pId;
+
+  RETURN packageIsEnabled(_pkgname);
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION packageIsEnabled(pName TEXT) RETURNS BOOLEAN AS $$
+-- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
-  SELECT COUNT(*) >= 8
-  FROM pg_inherits, pg_class, pg_namespace
-  WHERE ((inhrelid=pg_class.oid)
-     AND (relnamespace=pg_namespace.oid)
-     AND  (nspname=lower($1)));
-$$
-LANGUAGE 'sql';
+DECLARE
+  _result BOOLEAN;
+
+BEGIN
+
+  SELECT BOOL_OR(parent.relname IN ('cmd',  'cmdarg', 'image',  'metasql', 'priv', 'report', 'script', 'uiform', 'dict')) INTO _result
+    FROM pg_inherits
+    JOIN pg_class parent ON inhparent=parent.oid
+    JOIN pg_class child ON inhrelid=child.oid
+    JOIN pg_namespace ON child.relnamespace=pg_namespace.oid
+   WHERE nspname=pName;
+
+   RETURN COALESCE(_result, FALSE);
+
+END;
+$$ LANGUAGE plpgsql;
