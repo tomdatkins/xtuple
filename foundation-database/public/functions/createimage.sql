32f56d38170b6ce97379a5fc2e17ea82e89310e0
Dave Anderson (xTuple) <danderson@xtuple.com>
2017-09-12 13:42:55 +1200

    Correct function typo

--- foundation-database/public/functions/createimage.sql
+++ foundation-database/public/functions/createimage.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE FUNCTION public.createimage(
   RETURNS integer AS $$
 -- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
-DECVLARE
+DECLARE
   _id integer;
 BEGIN
   INSERT INTO image (image_id, image_name, image_descrip, image_data) 
