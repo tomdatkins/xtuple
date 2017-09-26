a8c1cc1f591a89c865503c5647d1af58a9c75202
Dave Anderson (xTuple) <danderson@xtuple.com>
2017-09-26 14:44:17 +1300

    Update commercialcore Mobile Web manifest

--- source/commercialcore/database/source/manifest.js
+++ source/commercialcore/database/source/manifest.js
@@ -5,10 +5,10 @@
   "loadOrder": 50,
   "dependencies": [],
   "databaseScripts": [
-    "../../foundation-database/workflow/sql/trigger_functions/createwf_triggers.sql",
-    "../../foundation-database/workflow/scripts/initMenu.js",
-    {"path": "../../foundation-database/workflow/scripts/setup.js", "order": 10},
-    "../../foundation-database/workflow/sql/update_metric.sql"
+    "../../foundation-database/xtcore/trigger_functions/createwf_triggers.sql",
+    "../../foundation-database/xtcore/tables/pkgscript/initMenu.js",
+    {"path": "../../foundation-database/xtcore/tables/pkgscript/setup.js", "order": 10},
+    "../../foundation-database/xtcore/update_metric.sql"
   ]
 }
 
