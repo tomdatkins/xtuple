6aaa46d667be5a96b0cd53653e43fa1bd4e3f101
jcarlin <jcarlin@xtuple.com>
2017-10-02 17:00:07 -0500

    columns addeded to invhist, not invdetail: correct function back to using invhist_orditem_id

--- foundation-database/public/functions/getlotserialids.sql
+++ foundation-database/public/functions/getlotserialids.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE FUNCTION getLotSerialIds(pOrdType TEXT DEFAULT NULL,
                                            pOrdItemId INTEGER DEFAULT NULL,
                                            pItemsiteId INTEGER DEFAULT NULL,
                                            pItemlocdistId INTEGER DEFAULT NULL) RETURNS INTEGER[] AS $$
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.    
+-- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.    
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE 
   _lsIds INTEGER[];
@@ -11,15 +11,16 @@ DECLARE
 BEGIN
 
   IF (COALESCE(pOrdType, '') <> '') THEN
-    IF (pOrdItemId IS NULL) THEN
-      RAISE EXCEPTION 'pOrdItemId param is required when passing pOrdType. [xtuple: getLotSerialIds, -1]';
+    IF (pOrdHeadId IS NULL OR pOrdItemId IS NULL) THEN
+      RAISE EXCEPTION 'pOrdHeadId OR pOrdItemId param is required when passing pOrdType. [xtuple: getLotSerialIds, -1]';
     END IF;
 
     _lsIds := _lsIds || ARRAY(
       SELECT ls_id
       FROM invdetail
         JOIN invhist ON invdetail_invhist_id=invhist_id AND invhist_ordtype=pOrdType
-      WHERE true
+      WHERE invdetail_ls_id IS NOT NULL
+        AND CASE WHEN pOrdHeadId IS NOT NULL THEN invdetail_ordhead_id=pOrdHeadId END
         AND CASE WHEN pOrdItemId IS NOT NULL THEN invdetail_orditem_id=pOrdItemId END
     );
   
@@ -32,7 +33,8 @@ BEGIN
       WHERE itemloc_itemsite_id=pItemsiteId
     );
 
-  -- Use case: user is transacting this right now and has the Create Lot Serial screen open, look for any original/outgoing inventory transactions
+  -- Use case: user is transacting this right now and has the Create Lot Serial screen open,
+  --  look for any original/outgoing inventory transactions' lot/serial record(s)
   ELSIF (pItemlocdistId IS NOT NULL) THEN
 
     SELECT * INTO _r
@@ -50,10 +52,9 @@ BEGIN
         WHERE invhist_id=_r.itemlocdist_invhist_id
       );
     END IF;
-
-    -- TODO - support for Credit Memos, Invoices, ?
     
     -- The following inv trans types should support finding the original transaction and it's ls record(s)
+    -- TODO - support for Credit Memos, Invoices, ?.
 
     -- Receiving an RA return. Get the ls records from original SO Issue to Shipping
     IF (_r.itemlocdist_order_type = 'RA' AND _r.itemlocdist_transtype = 'RR') THEN
@@ -71,10 +72,11 @@ BEGIN
       _lsIds := _lsIds || ARRAY(
         SELECT ls_id 
         FROM coitem
-          JOIN invdetail ON coitem_id=invdetail_orditem_id
-          JOIN invhist ON invdetail_invhist_id=invhist_id AND invhist_transtype = 'SH' AND invhist_ordtype = 'SO'
+          JOIN invhist ON coitem_id=invhist_orditem_id AND invhist_transtype = 'SH' AND invhist_ordtype = 'SO'
+          JOIN invdetail ON invhist_id=invdetail_invhist_id
           JOIN ls ON invdetail_ls_id=ls_id
         WHERE coitem_id=_r.itemlocdist_order_id
+          AND invdetail_ls_id IS NOT NULL
       );
     
     -- Returning to Shipping (TO). Get the ls records from original TO Issue to Shipping
@@ -82,8 +84,8 @@ BEGIN
       _lsIds := _lsIds || ARRAY(
         SELECT invdetail_ls_id 
         FROM toitem
-          JOIN invdetail ON toitem_id=invdetail_orditem_id
-          JOIN invhist ON invdetail_invhist_id=invhist_id AND invhist_transtype = 'SH' AND invhist_ordtype = 'TO'
+          JOIN invhist ON toitem_id=invhist_orditem_id AND invhist_transtype = 'SH' AND invhist_ordtype = 'TO'
+          JOIN invdetail ON invhist_id=invdetail_invhist_id
         WHERE toitem_id=_r.itemlocdist_order_id
           AND invdetail_ls_id IS NOT NULL
       );
@@ -93,8 +95,8 @@ BEGIN
       _lsIds := _lsIds || ARRAY(
         SELECT invdetail_ls_id 
         FROM womatl
-          JOIN invdetail ON womatl_id=invdetail_orditem_id 
-          JOIN invhist ON invdetail_invhist_id=invhist_id AND invhist_transtype = 'IM' AND invhist_ordtype = 'WO'
+          JOIN invhist ON womatl_id=invhist_orditem_id AND invhist_transtype = 'IM' AND invhist_ordtype = 'WO'
+          JOIN invdetail ON invhist_id=invdetail_invhist_id
         WHERE womatl_id=_r.itemlocdist_order_id
           AND invdetail_ls_id IS NOT NULL
       );
@@ -104,8 +106,8 @@ BEGIN
       _lsIds := _lsIds || ARRAY(
         SELECT ls_id 
         FROM toitem
-          JOIN invdetail ON toitem=invdetail_orditem_id
-          JOIN invhist ON invdetail_invhist_id=invhist_id AND invhist_transtype = 'SH' AND invhist_ordtype = 'TO'
+          JOIN invhist ON toitem_id=invhist_orditem_id AND invhist_transtype = 'SH' AND invhist_ordtype = 'TO'
+          JOIN invdetail ON invhist_id=invdetail_invhist_id
         WHERE toitem_id=_r.itemlocdist_order_id
           AND invdetail_ls_id IS NOT NULL
       );
@@ -119,9 +121,8 @@ BEGIN
       pOrdHeadId: %
       pOrdItemId: %
       pItemsiteId: %
-      pItemlocdistId: %
-      [xtuple: getLotSerialIds, -3, %, %, %, %, %].',
-      pOrdType, pOrdHeadId, pOrdItemId, pItemsiteId, pItemlocdistId, pOrdType, pOrdHeadId, pOrdItemId, pItemsiteId, pItemlocdistId;
+      pItemlocdistId: %',
+      pOrdType, pOrdHeadId, pOrdItemId, pItemsiteId, pItemlocdistId;
     RETURN NULL;
   ELSE 
     -- Return distinct values in array
@@ -132,4 +133,4 @@ END;
 
 $$ LANGUAGE plpgsql;
 
-COMMENT ON FUNCTION getLotSerialIds(TEXT, INTEGER, INTEGER, INTEGER, INTEGER) IS 'Returns an array of ls_ids - when pItemlocdistId passed: return ls_ids tied to the current transaction.';
+COMMENT ON FUNCTION getLotSerialIds(TEXT, INTEGER, INTEGER, INTEGER, INTEGER) IS 'Used in createLotSerial.cpp. Returns an array of ls_ids. When pItemlocdistId passed: return ls_ids from the original/outgoing inventory transaction.';
