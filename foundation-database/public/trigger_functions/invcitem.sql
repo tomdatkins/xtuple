ec719184a622c0028abfe56b830ad6e9f034ef30
Dave Anderson (xTuple) <danderson@xtuple.com>
2017-10-05 10:57:09 +1300

    Additional Update Comments based on further customer feedback

--- foundation-database/public/trigger_functions/invcitem.sql
+++ foundation-database/public/trigger_functions/invcitem.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION _invcitemBeforeTrigger() RETURNS "trigger" AS $$
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+-- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   _itemfractional BOOLEAN;
@@ -46,7 +46,7 @@ CREATE TRIGGER invcitemBeforeTrigger
   EXECUTE PROCEDURE _invcitemBeforeTrigger();
 
 CREATE OR REPLACE FUNCTION _invcitemTrigger() RETURNS trigger AS $$
--- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple. 
+-- Copyright (c) 1999-2017 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   _r RECORD;
@@ -95,6 +95,41 @@ BEGIN
                                 COALESCE(_r.invchead_curr_id, -1),
                                 (NEW.invcitem_billed * NEW.invcitem_qty_invuomratio) *
                                 (NEW.invcitem_price / NEW.invcitem_price_invuomratio) );
+    END IF;
+
+  -- Record Changes
+    IF (NEW.invcitem_billed <> OLD.invcitem_billed) THEN
+       PERFORM postComment('ChangeLog', 'INVI', NEW.invcitem_id, 'Billed Qty',
+                            formatQty(OLD.invcitem_billed), formatQty(NEW.invcitem_billed));
+    END IF;
+    IF (NEW.invcitem_price <> OLD.invcitem_price) THEN
+      PERFORM postComment('ChangeLog', 'INVI', NEW.invcitem_id, 'Price',
+                           formatPrice(OLD.invcitem_price), formatPrice(NEW.invcitem_price));
+    END IF;
+    IF (NEW.invcitem_taxtype_id <> OLD.invcitem_taxtype_id) THEN
+      PERFORM postComment('ChangeLog', 'INVI', NEW.invcitem_id, 'Tax Type',
+                          (SELECT taxtype_name FROM taxtype WHERE taxtype_id=OLD.invcitem_taxtype_id),
+                          (SELECT taxtype_name FROM taxtype WHERE taxtype_id=NEW.invcitem_taxtype_id));
+    END IF;
+    IF (NEW.invcitem_rev_accnt_id <> OLD.invcitem_rev_accnt_id) THEN
+      PERFORM postComment('ChangeLog', 'INVI', NEW.invcitem_id, 'Revenue Account',
+                          (SELECT accnt_name FROM accnt WHERE accnt_id=OLD.invcitem_rev_accnt_id),
+                          (SELECT accnt_name FROM accnt WHERE accnt_id=NEW.invcitem_rev_accnt_id));
+    END IF;
+    IF (OLD.invcitem_item_id <> -1 AND NEW.invcitem_salescat_id > 0) THEN
+      PERFORM postComment('ChangeLog', 'INVI', NEW.invcitem_id, 'Switched Item to Misc:',
+                          (SELECT item_number FROM item WHERE item_id=OLD.invcitem_item_id),
+                          (SELECT salescat_name FROM salescat WHERE salescat_id=NEW.invcitem_salescat_id));
+    END IF;
+    IF (OLD.invcitem_salescat_id > 0 AND NEW.invcitem_item_id <> -1) THEN
+      PERFORM postComment('ChangeLog', 'INVI', NEW.invcitem_id, 'Switched Misc to Item:',
+                          (SELECT salescat_name FROM salescat WHERE salescat_id=OLD.invcitem_salescat_id),
+                          (SELECT item_number FROM item WHERE item_id=NEW.invcitem_item_id));
+    END IF;
+    IF (OLD.invcitem_salescat_id > 0 AND NEW.invcitem_salescat_id <> OLD.invcitem_salescat_id) THEN
+      PERFORM postComment('ChangeLog', 'INVI', NEW.invcitem_id, 'Sales Category',
+                          (SELECT salescat_name FROM salescat WHERE salescat_id=OLD.invcitem_salescat_id),
+                          (SELECT salescat_name FROM salescat WHERE salescat_id=NEW.invcitem_salescat_id));
     END IF;
   END IF;
 
