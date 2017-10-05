0901d9feed1a4b0c46ffc915195f5849cf8f7c95
Gil Moskowitz <gilmoskowitz@acm.org>
2017-10-05 16:57:34 -0400

    move quality from private-extensions to xtquality repo

--- source/quality/foundation-database/source/public/tables/wooper.sql
+++
@@ -1,5 +0,0 @@
-drop trigger if exists wooperqualitytrigger on xtmfg.wooper;
-
-CREATE TRIGGER wooperqualitytrigger AFTER INSERT
-  ON xtmfg.wooper FOR EACH ROW
-  EXECUTE PROCEDURE xt.triggerwooperquality();
--- source/quality/foundation-database/source/xt/tables/wftype.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM xt.wftype WHERE wftype_code = 'QU';
-
-INSERT INTO xt.wftype (wftype_table, wftype_tblname, wftype_code, wftype_src_tblname, wftype_uuid_col, 
-            wftype_parentid_col, wftype_id_col, wftype_number_col ) 
-VALUES ('qthead', 'qualitytestwf', 'QU', 'qualityplanwf', 'obj_uuid', 'qthead_qphead_id', 'qthead_id', 'qthead_number'); 
