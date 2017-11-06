6855a57aaf22d8417a0e4c14731939aa14b78e85
Chris Cotnoir <ccotnoir@xtuple.com>
2017-11-06 09:41:14 -0500

    Address comments

--- foundation-database/public/patches/migrateccpay.sql
+++ foundation-database/public/patches/migrateccpay.sql
@@ -3,14 +3,11 @@ DECLARE
   _r RECORD;
   _resultcashrcpt INTEGER;
   _resultccpay INTEGER;
-  _cashrcpts INTEGER[];
   _ccpays INTEGER[];
   _foundcashrcpts INTEGER[];
   _foundccpays INTEGER[];
-  _cashrcpt INTEGER;
+  _cashrcpt RECORD;
   _ccpay INTEGER;
-  _amount NUMERIC;
-  _currid INTEGER;
 
 BEGIN
 
@@ -22,18 +19,18 @@ BEGIN
                         THEN TEXT(cashrcpt_id)
                         ELSE cashrcpt_docnumber END AS docnumber,
                    cashrcpt_cust_id,
-                   COUNT(*) AS count
+                   array_agg(cashrcpt_id) AS ids
               FROM cashrcpt
              WHERE isExternalFundsType(cashrcpt_fundstype)
                AND cashrcpt_ccpay_id IS NULL
              GROUP BY docnumber, cashrcpt_cust_id
   LOOP
-    IF _r.count=1 THEN
+    IF array_length(_r.ids, 1)=1 THEN
       -- Only one cashrcpt. Pick a matching ccpay by following priority:
-      -- 1. Matching currency and amount
-      -- 2. Matching currency only
-      -- 3. Matching amount
-      -- 4. Any
+      -- 3. Matching currency and amount (bitmask 11)
+      -- 2. Matching currency only (bitmask 10)
+      -- 1. Matching amount (bitmask 01)
+      -- 0. Any (bitmask 00)
       -- When choosing among these, try to match based on creation time, approximated
       -- by cashrcpt_id for cashrcpt and done by ccpay_transaction_datetime followed by ccpay_id
       -- for ccpay
@@ -57,10 +54,7 @@ BEGIN
                                          ORDER BY match DESC,
                                          ccpay_transaction_datetime, ccpay_id) ordered
                                  LIMIT 1)
-       WHERE CASE WHEN TRIM(COALESCE(cashrcpt_docnumber, ''))=''
-                             THEN TEXT(cashrcpt_id)
-                             ELSE cashrcpt_docnumber END=_r.docnumber
-             AND cashrcpt_cust_id=_r.cashrcpt_cust_id
+       WHERE cashrcpt_id=_r.ids[1]
        RETURNING cashrcpt_id, cashrcpt_ccpay_id
             INTO _resultcashrcpt, _resultccpay;
 
@@ -69,13 +63,6 @@ BEGIN
     ELSE
       -- Multiple cash rcecipts. Grab all cash receipts and ccpays with this number and try
       -- to match them based on amount, currency, and order as above
-      _cashrcpts := ARRAY(SELECT cashrcpt_id
-                            FROM cashrcpt
-                           WHERE CASE WHEN TRIM(COALESCE(cashrcpt_docnumber, ''))=''
-                                      THEN TEXT(cashrcpt_id)
-                                      ELSE cashrcpt_docnumber END=_r.docnumber
-                             AND cashrcpt_cust_id=_r.cashrcpt_cust_id
-                           ORDER BY cashcrpt_id);
       _ccpays := ARRAY(SELECT ccpay_id
                          FROM ccpay
                         WHERE ccpay_type='C'
@@ -84,91 +71,20 @@ BEGIN
                           AND ccpay_cust_id=_r.cashrcpt_cust_id
                         ORDER BY ccpay_transaction_datetime, ccpay_id);
 
-      FOREACH _cashrcpt IN ARRAY _cashrcpts
+      FOR _cashrcpt IN SELECT cashrcpt_id, cashrcpt_amount, cashrcpt_curr_id,
+                             generate_series(3, 0, -1) AS match
+                        FROM cashrcpt
+                       WHERE cashrcpt_id IN (SELECT UNNEST(_r.ids))
+                       ORDER BY match DESC, cashrcpt_id
       LOOP
-        SELECT cashrcpt_amount, cashrcpt_curr_id
-          INTO _amount, _currid
-          FROM cashrcpt
-         WHERE cashrcpt_id=_cashrcpt;
-
-        FOREACH _ccpay IN ARRAY _ccpays
-        LOOP
-          IF ((SELECT ccpay_amount=_amount AND ccpay_curr_id=_currid
-                 FROM ccpay
-                WHERE ccpay_id=_ccpay)
-              AND _ccpay NOT IN (SELECT UNNEST(_foundccpays))) THEN
-            UPDATE cashrcpt
-               SET cashrcpt_ccpay_id=_ccpay
-             WHERE cashrcpt_id=_cashrcpt;
-
-            _foundcashrcpts := _foundcashrcpts || _cashrcpt;
-            _foundccpays := _foundccpays || _ccpay;
-
-            EXIT;
-          END IF;
-        END LOOP;
-      END LOOP;
-
-      FOREACH _cashrcpt IN ARRAY _cashrcpts
-      LOOP
-        IF (_cashrcpt NOT IN (SELECT UNNEST(_foundcashrcpts))) THEN
-          SELECT cashrcpt_amount
-            INTO _amount
-            FROM cashrcpt
-           WHERE cashrcpt_id=_cashrcpt;
-
+        IF (_cashrcpt.cashrcpt_id NOT IN (SELECT UNNEST(_foundcashrcpts))) THEN
           FOREACH _ccpay IN ARRAY _ccpays
           LOOP
-            IF ((SELECT ccpay_amount=_amount
+            IF ((SELECT ((match & 2)!=0 OR ccpay_curr_id=_r.cashrcpt_curr_id)
+                        AND ((match & 1)!=0 OR ccpay_amount=_cashrcpt.cashrcpt_amount)
                    FROM ccpay
                   WHERE ccpay_id=_ccpay)
                 AND _ccpay NOT IN (SELECT UNNEST(_foundccpays))) THEN
-              UPDATE cashrcpt
-                 SET cashrcpt_ccpay_id=_ccpay
-               WHERE cashrcpt_id=_cashrcpt;
-
-              _foundcashrcpts := _foundcashrcpts || _cashrcpt;
-              _foundccpays := _foundccpays || _ccpay;
-
-              EXIT;
-            END IF;
-          END LOOP;
-        END IF;
-      END LOOP;
-
-      FOREACH _cashrcpt IN ARRAY _cashrcpts
-      LOOP
-        IF (_cashrcpt NOT IN (SELECT UNNEST(_foundcashrcpts))) THEN
-          SELECT cashrcpt_curr_id
-            INTO _currid
-            FROM cashrcpt
-           WHERE cashrcpt_id=_cashrcpt;
-
-          FOREACH _ccpay IN ARRAY _ccpays
-          LOOP
-            IF ((SELECT ccpay_curr_id=_currid
-                   FROM ccpay
-                  WHERE ccpay_id=_ccpay)
-                AND _ccpay NOT IN (SELECT UNNEST(_foundccpays))) THEN
-              UPDATE cashrcpt
-                 SET cashrcpt_ccpay_id=_ccpay
-               WHERE cashrcpt_id=_cashrcpt;
-
-              _foundcashrcpts := _foundcashrcpts || _cashrcpt;
-              _foundccpays := _foundccpays || _ccpay;
-
-              EXIT;
-            END IF;
-          END LOOP;
-        END IF;
-      END LOOP;
-
-      FOREACH _cashrcpt IN ARRAY _cashrcpts
-      LOOP
-        IF (_cashrcpt NOT IN (SELECT UNNEST(_foundcashrcpts))) THEN
-          FOREACH _ccpay IN ARRAY _ccpays
-          LOOP
-            IF (_ccpay NOT IN (SELECT UNNEST(_foundccpays))) THEN
               UPDATE cashrcpt
                  SET cashrcpt_ccpay_id=_ccpay
                WHERE cashrcpt_id=_cashrcpt;
