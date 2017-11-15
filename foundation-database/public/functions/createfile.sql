4af6659c2a4b1a69f7a77f908f8e6d32888f41d2
Chris Cotnoir <ccotnoir@xtuple.com>
2017-11-15 10:29:07 -0500

    Address comments

--- foundation-database/public/functions/createfile.sql
+++ foundation-database/public/functions/createfile.sql
@@ -3,7 +3,7 @@ declare
   _id integer;
 begin
   _id := nextval('file_file_id_seq');
-  insert into file (file_id, file_title, file_descrip, file_stream, file_mime_type) values (_id, pTitle, pDescription, pStream, pMimeType);
+  insert into file (file_id, file_title, file_descrip, file_stream, file_mime_type) values (_id, pTitle, pDescription, pStream, COALESCE(pMimeType, 'application/octet-stream'));
   return _id;
 end;
 $$ language 'plpgsql';
